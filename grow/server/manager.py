b7d29d351de16ac30ab9436a5135bbd213ce6f45
jeremydw <jeremydw@grow.io>
2013-10-02 08:09:54 -0700

    Add podserver object.

--- grow/server/manager.py
+++ grow/server/manager.py
@@ -7,8 +7,10 @@ from paste import httpserver
 import atexit
 import multiprocessing
 import os
+import yaml
 
 _servers = {}
+_config_path = '{}/.grow/servers'.format(os.environ['HOME'])
 
 
 def start(root, port=None, use_subprocess=False):
@@ -39,3 +41,44 @@ def stop(root):
 def stop_all():
   for root in _servers.keys():
     stop(root)
+
+
+def read_config():
+  return yaml.load(_config_path)
+
+
+def write_config(config):
+  path = os.path.dirname(_config_path)
+  if not os.path.exists(path):
+    os.makedirs(path)
+  content = yaml.dump(config, default_flow_style=False)
+  fp = open(_config_path, 'w')
+  fp.write(content)
+  fp.close()
+
+
+class Pod(object):
+
+  def __init__(self, root, port=8000):
+    self.root = root
+    self.port = port
+
+  @property
+  def revision_status(self):
+    pass
+
+  @property
+  def server_status(self):
+    return 'off'
+
+  def start_server(self):
+    pass
+
+  def stop_server(self):
+    pass
+
+  def set_root(self, root):
+    self.root = root
+
+  def set_port(self, port):
+    self.port = port
