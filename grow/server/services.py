ff915fa0883bbd3d3537c46d698ad1f70d483cc8
jeremydw <jeremydw@grow.io>
2013-10-24 15:06:37 -0700

    Update services for subclassing.

--- grow/server/services.py
+++ grow/server/services.py
@@ -6,11 +6,18 @@ from grow.pods import pods
 
 class PodService(remote.Service):
 
+  def get_pod_from_request(self, request):
+    root = os.path.normpath(os.environ['grow:single_pod_root'])
+    return pods.Pod(root)
+
+  def get_document_from_request(self, pod, request):
+    return pod.get_document(request.document.pod_path)
+
   @remote.method(
       messages.ListBlueprintsRequest,
       messages.ListBlueprintsResponse)
   def list_blueprints(self, request):
-    pod = get_pod_from_request(request)
+    pod = self.get_pod_from_request(request)
     results = pod.list_blueprints()
     message = messages.ListBlueprintsResponse()
     message.blueprints = [blueprint.to_message() for blueprint in results]
@@ -20,7 +27,7 @@ class PodService(remote.Service):
       messages.SearchDocumentsRequest,
       messages.SearchDocumentsResponse)
   def search_documents(self, request):
-    pod = get_pod_from_request(request)
+    pod = self.get_pod_from_request(request)
     blueprint = pod.get_blueprint(request.blueprint.nickname)
     docs = blueprint.search()
     message = messages.SearchDocumentsResponse()
@@ -31,17 +38,8 @@ class PodService(remote.Service):
       messages.GetDocumentRequest,
       messages.GetDocumentResponse)
   def get_document(self, request):
-    pod = get_pod_from_request(request)
-    document = get_document_from_request(pod, request)
+    pod = self.get_pod_from_request(request)
+    document = self.get_document_from_request(pod, request)
     message = messages.GetDocumentResponse()
     message.document = document.to_message()
     return message
-
-
-def get_pod_from_request(request):
-  root = os.path.normpath(os.environ['grow:single_pod_root'])
-  return pods.Pod(root)
-
-
-def get_document_from_request(pod, request):
-  return pod.get_document(request.document.pod_path)
