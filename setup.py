f5271ff46e7fcc235e9e5f9691b11c1eb7f9e898
jeremydw <jeremydw@grow.io>
2014-02-14 16:50:41 -0800

    Update distribution.

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
+from setuptools import find_packages
 from setuptools import setup
 
 
 setup(
     name='grow',
     version=open('VERSION').read().strip(),
-    description='Develop everywhere and deploy anywhere: a static site generator/CMS that helps teams build high-quality web sites.',
+    description=(
+          'Develop everywhere and deploy anywhere: a static site generator/CMS'
+          ' that helps teams build high-quality web sites.'
+    ),
     long_description=open('description.txt').read().strip(),
     url='http://growsdk.org',
     license='MIT',
     author='Grow SDK Authors',
     author_email='hello@grow.io',
+    include_package_data=True,
+    packages=find_packages(
+        exclude=[
+            'grow/submodules',
+        ]
+    ),
     scripts=[
         'bin/grow',
     ],
@@ -22,11 +32,15 @@ setup(
         'content management'
     ],
     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ])
