2d2ae4e52b7e917676b0b89cef4289ed1bc54101
jeremydw <jeremydw@grow.io>
2014-03-07 16:54:36 -0800

    Remove crufty submodules related to Travis debugging.

--- grow/submodules/__init__.py
+++ grow/submodules/__init__.py
@@ -1,3 +1,5 @@
+# TODO(jeremydw): Reevaluate if we need this for non-standard distribution
+# (such as GAE, or a Mac application).
 import os
 import sys
 
@@ -6,26 +8,6 @@ def fix_imports():
   here = os.path.dirname(__file__)
   dirs = [
       os.path.normpath(os.path.join(here, '..', '..')),
-      os.path.normpath(os.path.join(here, 'Python-Markdown')),
-      os.path.normpath(os.path.join(here, 'appengine-gcs-client', 'python', 'src')),
-      os.path.normpath(os.path.join(here, 'babel')),
-      os.path.normpath(os.path.join(here, 'boto')),
-      os.path.normpath(os.path.join(here, 'dulwich')),
-      os.path.normpath(os.path.join(here, 'dnspython')),
-      os.path.normpath(os.path.join(here, 'google-api-python-client')),
-      os.path.normpath(os.path.join(here, 'google-apputils-python')),
-      os.path.normpath(os.path.join(here, 'httplib2', 'python2')),
-      os.path.normpath(os.path.join(here, 'jinja2')),
-      os.path.normpath(os.path.join(here, 'markupsafe')),
-      os.path.normpath(os.path.join(here, 'paste')),
-      os.path.normpath(os.path.join(here, 'premailer')),
-      os.path.normpath(os.path.join(here, 'protorpc-standalone', 'python')),
-      os.path.normpath(os.path.join(here, 'python-gflags')),
-      os.path.normpath(os.path.join(here, 'pytz')),
-      os.path.normpath(os.path.join(here, 'pyyaml', 'lib')),
-      os.path.normpath(os.path.join(here, 'requests')),
-      os.path.normpath(os.path.join(here, 'werkzeug')),
-      os.path.normpath(os.path.join(here, 'webob')),
   ]
   sys.path[1:1] = dirs
   return dirs
