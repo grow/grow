10769fab5bc426239c11a996b3c39d2d65abeee8
jeremydw <jeremydw@grow.io>
2014-03-29 14:17:41 -0700

    Prompt to run auto-installer when local version is behind released version. #19

--- grow/common/sdk_utils.py
+++ grow/common/sdk_utils.py
@@ -1,12 +1,14 @@
 from grow.common import config
 from grow.common import utils
+import json
 import logging
-import requests
-from requests.packages.urllib3 import connectionpool
+import os
+import subprocess
+import sys
+import urllib
 
-# Hide requests logging.
-connectionpool.log.setLevel(logging.WARNING)
 RELEASES_API = 'https://api.github.com/repos/grow/pygrow/releases'
+INSTALLER_COMMAND = '/usr/bin/python -c "$(curl -fsSL https://raw.github.com/grow/pygrow/master/install.py)"'
 
 
 class Error(Exception):
@@ -24,7 +26,7 @@ def get_this_version():
 def get_latest_version():
   logging.info('Checking for updates to the Grow SDK...')
   try:
-    releases = requests.get(RELEASES_API).json()
+    releases = json.loads(urllib.urlopen(RELEASES_API).read())
     return releases[0]['tag_name']
   except Exception as e:
     text = 'Could not check for updates to the SDK. Are you online?'
@@ -32,23 +34,25 @@ def get_latest_version():
     raise LatestVersionCheckError(str(e))
 
 
-def check_version(quiet=False):
+def check_version(auto_update_prompt=False):
   try:
     theirs = get_latest_version()
     yours = config.VERSION
-    if theirs > yours:
-      logging.info('---')
-      logging.info(utils.colorize('{green}Your version: %s,{/green} {yellow}latest version: %s{/yellow}' % (yours, theirs)))
-      logging.info('A newer version of the SDK is available, so please update yours. See http://growsdk.org.')
-      if utils.is_packaged_app():
-        logging.info('Quick update by pasting the following command:')
-        command = '  python -c "$(curl -fsSL https://raw.github.com/grow/pygrow/master/install.py)"'
-        logging.info(utils.colorize('{yellow}%s{/yellow}' % command))
-      else:
-        logging.info('Update using: pip install --upgrade grow')
-      logging.info('---')
-    else:
-      logging.info('You have the latest version: {}'.format(yours))
   except LatestVersionCheckError:
-    if not quiet:
-      raise
+    return
+
+  if theirs > yours:
+    logging.info('---')
+    logging.info(utils.colorize('{green}Your version: %s,{/green} {yellow}latest version: %s{/yellow}' % (yours, theirs)))
+    logging.info('A newer version of the SDK is available, so please update yours. See http://growsdk.org.')
+    if utils.is_packaged_app() and auto_update_prompt:
+      # If the installation was successful, restart the process.
+      if (raw_input('Auto update now? [y/N]: ').lower() == 'y'
+          and subprocess.call(INSTALLER_COMMAND, shell=True) == 0):
+        logging.info('Restarting...')
+        os.execl(sys.argv[0], *sys.argv)
+    else:
+      logging.info('Update using: pip install --upgrade grow')
+    logging.info('---')
+  else:
+    logging.info('You have the latest version: {}'.format(yours))
