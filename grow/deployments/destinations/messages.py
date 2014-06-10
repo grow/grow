8f772d3d71d04d33777b98e9de2c832a595cc8da
jeremydw <jeremydw@gmail.com>
2014-06-10 14:35:59 -0700

    Bump VERSION for new content and deployment capabilities.

--- grow/deployments/destinations/messages.py
+++ grow/deployments/destinations/messages.py
@@ -1,14 +1,6 @@
 from protorpc import messages
 
 
-class DeploymentKind(messages.Enum):
-  GOOGLE_STORAGE = 1
-  GOOGLE_STORAGE_FROM_APP_ENGINE = 2
-  AMAZON_S3 = 3
-  GROW_CHANNEL = 4
-  FILE_SYSTEM = 5
-
-
 class Result(messages.Enum):
   PASS = 1
   FAIL = 2
