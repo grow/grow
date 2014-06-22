6564c698894b98007a5cb8c177d7b629cfe70d02
jeremydw <jeremydw@gmail.com>
2014-06-22 14:22:46 -0700

    Add support for pygments syntax highlighting via [sourcecode] Markdown extension. Also, change Git destination path to root_dir to be equivalent with the local destination.

--- grow/common/markdown_extensions.py
+++ grow/common/markdown_extensions.py
@@ -1,6 +1,35 @@
+"""
+    The Pygments Markdown Preprocessor
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    This fragment is a Markdown_ preprocessor that renders source code
+    to HTML via Pygments.  To use it, invoke Markdown like so::
+
+        import markdown
+        html = markdown.markdown(someText, extensions=[CodeBlockExtension()])
+
+    This uses CSS classes by default, so use
+    ``pygmentize -S <some style> -f html > pygments.css``
+    to create a stylesheet to be added to the website.
+
+    You can then highlight source code in your markdown markup::
+
+        [sourcecode:lexer]
+        some code
+        [/sourcecode]
+
+    .. _Markdown: https://pypi.python.org/pypi/Markdown
+
+    :copyright: Copyright 2006-2014 by the Pygments team, see AUTHORS.
+    :license: BSD, see LICENSE for details.
+"""
+
 import re
 from markdown import extensions
 from markdown import preprocessors
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import get_lexer_by_name, TextLexer
 
 
 class IncludePreprocessor(preprocessors.Preprocessor):
@@ -72,3 +101,27 @@ class UrlExtension(extensions.Extension):
     # Adds the "include" preprocessor to the beginning of the list of preprocessors.
     # https://github.com/waylan/Python-Markdown/blob/master/markdown/odict.py#L7
     md.preprocessors.add('url', self.processor, '_begin')
+
+
+class CodeBlockPreprocessor(preprocessors.Preprocessor):
+  pattern = re.compile(r'\[sourcecode:(.+?)\](.+?)\[/sourcecode\]', re.S)
+  formatter = HtmlFormatter(noclasses=True)
+
+  def run(self, lines):
+    def repl(m):
+      try:
+        lexer = get_lexer_by_name(m.group(1))
+      except ValueError:
+        lexer = TextLexer()
+      code = highlight(m.group(2), lexer, self.formatter)
+      code = code.replace('\n\n', '\n&nbsp;\n').replace('\n', '<br />')
+      return '\n\n<div class="code">%s</div>\n\n' % code
+    joined_lines = "\n".join(lines)
+    joined_lines = self.pattern.sub(repl, joined_lines)
+    return joined_lines.split("\n")
+
+
+class CodeBlockExtension(extensions.Extension):
+
+  def extendMarkdown(self, md, md_globals):
+    md.preprocessors.add('CodeBlockPreprocessor', CodeBlockPreprocessor(), '_begin')
