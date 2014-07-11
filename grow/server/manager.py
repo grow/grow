1b216ab74107978a00606bd3c956a420e113efb3
Randy Merrill <RandyMerrill@google.com>
2014-07-11 16:52:54 -0700

    Pulling back to the upstream master version.
    Accidentally merged the wrong part of the conflict.

--- grow/server/manager.py
+++ grow/server/manager.py
@@ -44,7 +44,7 @@ class DevServerWSGIRequestHandler(simple_server.WSGIRequestHandler):
     sys.stderr.write('%s %s\n' % (timestring, format % args))
 
 
-def _start(pod, host=None, port=None, open_browser=False, debug=False):
+def start(pod, host=None, port=None, open_browser=False, debug=False):
   print ''
   print '  The Grow SDK is experimental. Expect backwards incompatibility until v0.1.0.'
   print '  Thank you for testing and contributing! Visit http://growsdk.org for resources.'
@@ -109,95 +109,3 @@ def _start(pod, host=None, port=None, open_browser=False, debug=False):
 
   # Clean up once server exits.
   sys.exit()
-
-
-def start(pod, host=None, port=None, open_browser=False, use_subprocess=False, debug=False):
-  root = pod.root
-  if root in _servers:
-    logging.error('Server already started for pod: {}'.format(root))
-    return
-
-  if not use_subprocess:
-    _start(pod, host=host, port=port, open_browser=open_browser, debug=debug)
-    return
-
-  server_process = multiprocessing.Process(target=_start, args=(root, host, port, open_browser, debug))
-  server_process.start()
-  _servers[root] = server_process
-  return server_process
-
-
-def stop(root):
-  process = _servers.pop(root, None)
-  if process is None:
-    return
-  try:
-    process.terminate()
-    logging.info('Stopped server for pod: {}'.format(root))
-  except AttributeError:
-    logging.info('Server already stopped for pod: {}'.format(root))
-
-
-@atexit.register
-def stop_all():
-  for root in _servers.keys():
-    stop(root)
-
-
-def write_config(config):
-  path = os.path.dirname(_config_path)
-  if not os.path.exists(path):
-    os.makedirs(path)
-  content = yaml.dump(config, default_flow_style=False)
-  fp = open(_config_path, 'w')
-  fp.write(content)
-  fp.close()
-
-
-class PodServer(object):
-
-  def __init__(self, root, port=8000, revision_status=None):
-    self.root = root
-    self.port = port
-    self.revision_status = revision_status
-    self.server_status = 'off'
-    self._process = None
-
-  def start(self):
-    self._process = start(self.root, port=self.port, use_subprocess=True)
-    self.server_status = 'on'
-    logging.info('Started server for pod: {}'.format(self.root))
-
-  def stop(self):
-    self.server_status = 'off'
-    try:
-      self._process.terminate()
-      logging.info('Stopped server for pod: {}'.format(self.root))
-    except AttributeError:
-      logging.info('Server already stopped.')
-
-  def set_root(self, root):
-    self.root = root
-
-  def set_port(self, port):
-    self.port = port
-
-  @property
-  def is_started(self):
-    return self.server_status == 'on'
-
-  @classmethod
-  def load(cls):
-    servers = []
-    try:
-      fp = open(_config_path)
-      for server in yaml.load(fp)['servers']:
-        servers.append(cls(
-            server['root'],
-            port=server.get('port'),
-            revision_status=server.get('revision_status')
-        ))
-    except IOError:
-      # .grow/servers.yaml does not exist.
-      pass
-    return servers
