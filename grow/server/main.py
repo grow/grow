06e99f31dd40634bc0944e9a0a4b5e4a55f21cb9
Randy Merrill <RandyMerrill@google.com>
2014-08-17 08:00:55 -0700

    Tweaking whitespace.

--- grow/server/main.py
+++ grow/server/main.py
@@ -11,12 +11,17 @@ from protorpc.wsgi import service
 from grow.server import handlers
 from grow.server import services
 
+
 podserver_app = webapp2.WSGIApplication([
     ('/.*', handlers.PodHandler),
 ])
+
+
 routes = [
     ('/_api/pods.*', services.PodService),
 ]
+
+
 application = service.service_mappings(
     routes,
     service_prefix='/_api',
