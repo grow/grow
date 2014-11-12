889a4e9898e99dd6165c90fb53b796f2741f53d0
jeremydw <jeremydw@gmail.com>
2014-11-12 11:07:49 -0800

    Handle PodNotFoundError for build.

--- grow/commands/build.py
+++ grow/commands/build.py
@@ -15,9 +15,12 @@ def build(pod_path, out_dir):
   root = os.path.abspath(os.path.join(os.getcwd(), pod_path))
   out_dir = out_dir or os.path.join(root, 'build')
   pod = pods.Pod(root, storage=storage.FileStorage)
-  paths_to_contents = pod.dump()
-  repo = utils.get_git_repo(pod.root)
-  config = local_destination.Config(out_dir=out_dir)
-  stats_obj = stats.Stats(pod, paths_to_contents=paths_to_contents)
-  destination = local_destination.LocalDestination(config, run_tests=False)
-  destination.deploy(paths_to_contents, stats=stats_obj, repo=repo, confirm=False)
+  try:
+    paths_to_contents = pod.dump()
+    repo = utils.get_git_repo(pod.root)
+    config = local_destination.Config(out_dir=out_dir)
+    stats_obj = stats.Stats(pod, paths_to_contents=paths_to_contents)
+    destination = local_destination.LocalDestination(config, run_tests=False)
+    destination.deploy(paths_to_contents, stats=stats_obj, repo=repo, confirm=False)
+  except pods.Error as e:
+    raise click.ClickException(str(e))
