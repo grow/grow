186522d98bc694ff85d6427324c719e56ac30c42
jeremydw <jeremydw@gmail.com>
2014-11-17 18:38:20 -0800

    Fix issue with deploy params.

--- grow/commands/deploy.py
+++ grow/commands/deploy.py
@@ -7,23 +7,20 @@ import os
 
 
 @click.command()
-@click.argument('pod_path', default='.')
 @click.argument('deployment_name', default='default')
+@click.argument('pod_path', default='.')
 @click.option('--skip_confirm', default=False, is_flag=True,
               help='Skip confirm prior to deployment.')
 @click.option('--test_only', default=False, is_flag=True,
               help='Only run the deployment tests.')
-def deploy(pod_path, deployment_name, skip_confirm, test_only):
+def deploy(deployment_name, pod_path, skip_confirm, test_only):
   """Deploys a pod to a destination."""
   root = os.path.abspath(os.path.join(os.getcwd(), pod_path))
-
   pod = pods.Pod(root, storage=storage.FileStorage)
   pod.preprocess()
-
   # Set the environment information for the pod based on the deployment.
   deployment = pod.get_deployment(deployment_name)
   pod.env = deployment.get_env()
-
   if test_only:
     deployment.test()
   else:
