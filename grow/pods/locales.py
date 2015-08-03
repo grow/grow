f447ffa36cdaa5155fa3eca98600600ac35465f6
Jeremy Weinstein <jeremydw@gmail.com>
2015-08-02 17:59:21 -0700

    Initial support for fuzzy locales. #93

--- grow/pods/locales.py
+++ grow/pods/locales.py
@@ -1,7 +1,10 @@
-import babel
-import re
+from babel import localedata
 from grow.pods import errors
 from grow.pods import messages
+import pickle
+import os
+import babel
+import re
 
 
 
@@ -55,6 +58,16 @@ class Locale(babel.Locale):
       language = '{}_{}'.format(parts[0], parts[1].upper())
     super(Locale, self).__init__(language, *args, **kwargs)
 
+  @classmethod
+  def parse(cls, *args, **kwargs):
+    locale = super(Locale, cls).parse(*args, **kwargs)
+    # Weak attempt to permit fuzzy locales (locales for which we still have
+    # language and country information, but not a full localedata file for),
+    # but disallow completely invalid locales. See note at end of file.
+    if locale.get_display_name() is None:
+      raise ValueError('{} is not a valid locale identifier'.format(args[0]))
+    return locale
+
   def __hash__(self):
     return hash(str(self))
 
@@ -103,3 +116,65 @@ class Locale(babel.Locale):
   @alias.setter
   def alias(self, alias):
     self._alias = alias
+
+
+# NOTE: Babel does not support "fuzzy" locales. A locale is considered "fuzzy"
+# when a corresponding "localedata" file that matches a given locale's full
+# identifier (e.g. "en_US") does not exist. Here's one example: "en_BD". CLDR
+# does not have a localedata file matching "en_BD" (English in Bangladesh), but
+# it does have individual files for "en" and also "bn_BD". As it turns
+# out, localedata files that correspond to a locale's full identifier (e.g.
+# "bn_BD.dat") are actually pretty light on the content (largely containing
+# things like start-of-week information) and most of the "meat" of the data is
+# contained in the main localedata file, e.g. "en.dat".
+#
+# Users may need to generate pages corresponding to locales that we don't
+# have full localedata for, and until Babel supports fuzzy locales, we'll
+# monkeypatch two Babel functions to provide partial support for fuzzy locales.
+#
+# With this monkeypatch, locales will be valid even if Babel doesn't have a
+# localedata file matching a locale's full identifier, but locales will still
+# fail with a ValueError if the user specifies a territory that does not exist.
+# With this patch, a user can, however, specify an invalid language. Obviously,
+# this patch should be removed when/if Babel adds support for fuzzy locales.
+# Optionally, we may want to provide users with more control over whether a
+# locale is valid or invalid, but we can revisit that later.
+
+# See: https://github.com/grow/pygrow/issues/93
+
+
+def fuzzy_load(name, merge_inherited=True):
+  localedata._cache_lock.acquire()
+  try:
+    data = localedata._cache.get(name)
+    if not data:
+      # Load inherited data
+      if name == 'root' or not merge_inherited:
+        data = {}
+      else:
+        parts = name.split('_')
+        if len(parts) == 1:
+          parent = 'root'
+        else:
+          parent = '_'.join(parts[:-1])
+        data = fuzzy_load(parent).copy()
+      filename = os.path.join(localedata._dirname, '%s.dat' % name)
+      try:
+        fileobj = open(filename, 'rb')
+        try:
+          if name != 'root' and merge_inherited:
+            localedata.merge(data, pickle.load(fileobj))
+          else:
+            data = pickle.load(fileobj)
+          localedata._cache[name] = data
+        finally:
+          fileobj.close()
+      except IOError:
+        pass
+    return data
+  finally:
+    localedata._cache_lock.release()
+
+
+localedata.exists = lambda name: True
+localedata.load = fuzzy_load
--- grow/pods/locales_test.py
+++ grow/pods/locales_test.py
@@ -1,5 +1,4 @@
 from . import locales
-import os
 import unittest
 
 
@@ -11,6 +10,17 @@ class LocalesTest(unittest.TestCase):
     self.assertEqual(locale, 'en_us')
     self.assertEqual(locale, locales.Locale('en_US'))
     self.assertEqual(locale, locales.Locale('en_us'))
+
+  def test_fuzzy_locales(self):
+    locale = locales.Locale.parse('en_BD')
+    self.assertEqual('en', locale.language)
+    self.assertEqual('BD', locale.territory)
+    self.assertEqual('English (Bangladesh)', locale.get_display_name())
+    locale = locales.Locale.parse('en_INVALID')
+    self.assertEqual('en', locale.language)
+    self.assertEqual(None, locale.territory)
+    self.assertRaises(ValueError, locales.Locale.parse, 'invalid')
+    self.assertRaises(ValueError, locales.Locale.parse, 'invalid_US')
 
 
 if __name__ == '__main__':
