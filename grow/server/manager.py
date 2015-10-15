8e2313b57f0c2e20bf03b0981a13cd2debcb4e92
Jeremy Weinstein <jeremydw@gmail.com>
2015-10-15 02:25:03 -0700

    Replace simple_server with Twisted. #60

--- grow/server/manager.py
+++ grow/server/manager.py
@@ -3,46 +3,14 @@ logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 from grow.pods.preprocessors import file_watchers
 from grow.server import main as main_lib
-from wsgiref import simple_server
+from twisted.internet import reactor
+from twisted.web import server
+from twisted.web import wsgi
 from xtermcolor import colorize
-import socket
 import sys
 import threading
-import time
+import twisted
 import webbrowser
-
-
-class DevServerWSGIRequestHandler(simple_server.WSGIRequestHandler):
-  _logging_enabled = False
-
-  @staticmethod
-  def sizeof(num):
-    for x in ['b', 'KB', 'MB', 'GB', 'TB']:
-      if num < 1024.0:
-        return '%3.1f%s' % (num, x)
-      num /= 1024.0
-
-  def log_date_time_string(self):
-    now = time.time()
-    year, month, day, hh, mm, ss, x, y, z = time.localtime(now)
-    return '%02d:%02d:%02d' % (hh, mm, ss)
-
-  def log_request(self, code=0, size='-'):
-    if not self._logging_enabled:
-      return
-    line = self.requestline[:-9]
-    method, line = line.split(' ', 1)
-    color = 19
-    if int(code) >= 500:
-      color = 161
-    code = colorize(code, ansi=color)
-    size = colorize(DevServerWSGIRequestHandler.sizeof(size), ansi=19)
-    self.log_message('{} {} {}'.format(code, line, size))
-
-  def log_message(self, format, *args):
-    #  def log_error(self, format, *args)
-    timestring = colorize(self.log_date_time_string(), ansi=241, ansi_bg=233)
-    sys.stderr.write('%s %s\n' % (timestring, format % args))
 
 
 def start(pod, host=None, port=None, open_browser=False, debug=False,
@@ -52,59 +20,56 @@ def start(pod, host=None, port=None, open_browser=False, debug=False,
     # Run preprocessors for the first time in a thread.
     thread = threading.Thread(target=pod.preprocess)
     thread.start()
+  port = 8080 if port is None else int(port)
+  host = 'localhost' if host is None else host
+  port = find_port_and_start_server(pod, host, port, debug)
+  url = print_server_ready_message(pod, host, port)
+  if open_browser:
+    start_browser(url)
+  reactor.run()
+  pod.logger.info('Goodbye. Shutting down.')
 
-  try:
-    # Create the development server.
-    app = main_lib.CreateWSGIApplication(pod, debug=debug)
-    port = 8080 if port is None else int(port)
-    host = 'localhost' if host is None else host
-    num_tries = 0
-    while num_tries < 10:
-      try:
-        handler_class = DevServerWSGIRequestHandler
-        httpd = simple_server.make_server(host, port, app,
-                                          handler_class=handler_class)
-        num_tries = 99
-      except socket.error as e:
-        if e.errno == 48:
-          num_tries += 1
-          port += 1
-        else:
-          raise e
 
-  except Exception as e:
-    pod.logger.error('Failed to start server: {}'.format(e))
-    observer.stop()
-    observer.join()
-    sys.exit()
+def find_port_and_start_server(pod, host, port, debug):
+  # Thread pool must be size 1 until a hard-to-pin down bug involving
+  # thread safety and static asset responses is fixed.
+  reactor.suggestThreadPoolSize(1)
+  app = main_lib.CreateWSGIApplication(pod, debug=debug)
+  num_tries = 0
+  while num_tries < 10:
+    try:
+      wsgi_resource = wsgi.WSGIResource(reactor, reactor.getThreadPool(), app)
+      reactor.listenTCP(port, server.Site(wsgi_resource), interface=host)
+      return port
+    except twisted.internet.error.CannotListenError as e:
+      if 'Errno 48' in str(e):
+        num_tries += 1
+        port += 1
+      else:
+        raise e
+  pod.logger.error('Unable to bind to {}:{}'.format(host, port))
+  sys.exit(-1)
 
-  try:
-    home_doc = pod.get_home_doc()
-    if home_doc:
-      root_path = home_doc.url.path
-    else:
-      root_path = pod.get_root_path()
-    url = 'http://{}:{}{}'.format(host, port, root_path)
-    print 'Pod: '.rjust(20) + pod.root
-    print 'Address: '.rjust(20) + url
-    print colorize('Server ready. '.rjust(20), ansi=47) + 'Press ctrl-c to quit.'
-    def start_browser(server_ready_event):
-      server_ready_event.wait()
-      if open_browser:
-        webbrowser.open(url)
-    server_ready_event = threading.Event()
-    browser_thread = threading.Thread(target=start_browser,
-                                      args=(server_ready_event,))
-    browser_thread.start()
-    server_ready_event.set()
-    httpd.serve_forever()
-    browser_thread.join()
 
-  except KeyboardInterrupt:
-    pod.logger.info('Goodbye. Shutting down.')
-    httpd.server_close()
-    observer.stop()
-    observer.join()
+def print_server_ready_message(pod, host, port):
+  home_doc = pod.get_home_doc()
+  if home_doc:
+    root_path = home_doc.url.path
+  else:
+    root_path = pod.get_root_path()
+  url = 'http://{}:{}{}'.format(host, port, root_path)
+  print 'Pod: '.rjust(20) + pod.root
+  print 'Address: '.rjust(20) + url
+  print colorize('Server ready. '.rjust(20), ansi=47) + 'Press ctrl-c to quit.'
+  return url
 
-  # Clean up once server exits.
-  sys.exit()
+
+def start_browser(url):
+  def _start_browser(server_ready_event):
+    server_ready_event.wait()
+    webbrowser.open(url)
+  server_ready_event = threading.Event()
+  browser_thread = threading.Thread(
+      target=_start_browser, args=(server_ready_event,))
+  browser_thread.start()
+  server_ready_event.set()
