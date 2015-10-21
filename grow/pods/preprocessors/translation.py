9cc1f567dc11d2caee02137a5fd86b1696e40135
Jeremy Weinstein <jeremydw@gmail.com>
2015-10-20 18:49:15 -0700

    Automatically restart the server when translation changes occur. Adjust file watchers to mask fewer errors. #14

--- grow/pods/preprocessors/translation.py
+++ grow/pods/preprocessors/translation.py
@@ -1,6 +1,8 @@
 from grow.pods.preprocessors import base
 from xtermcolor import colorize
+import os
 import re
+import sys
 
 SUFFIXES = frozenset(['po'])
 SUFFIX_PATTERN = re.compile('[.](' + '|'.join(map(re.escape, SUFFIXES)) + ')$')
@@ -13,14 +15,9 @@ class TranslationPreprocessor(base.BasePreprocessor):
   def __init__(self, pod):
     self.pod = pod
 
-  def first_run(self):
-    self.pod.catalogs.compile()
-
   def run(self):
-    # TODO(jeremydw): Ideally, this would be capable of flushing the gettext cache and
-    # recompiling the translations itself without requiring user action.
-    text = 'Detected changes to translations. Restart the server to see changes.'
-    print colorize(text, ansi=226)
+    self.pod.logger.info('Detected changes to translations. Restarting.')
+    os.execl(sys.argv[0], *sys.argv)
 
   def list_watched_dirs(self):
     return ['/translations/']
