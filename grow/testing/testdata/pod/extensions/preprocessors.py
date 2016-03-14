c3f8069435f0f1c09c00ed6dba2e4f3bdb7ab91b
Jeremy Weinstein <jeremydw@gmail.com>
2016-03-14 10:56:13 -0700

    Update extension testdata to take **kwargs.

--- grow/testing/testdata/pod/extensions/preprocessors.py
+++ grow/testing/testdata/pod/extensions/preprocessors.py
@@ -8,6 +8,6 @@ class CustomPreprocessor(Preprocessor):
     class Config(messages.Message):
         value = messages.StringField(1)
 
-    def run(self):
+    def run(self, **kwargs):
         # To allow the test to check the result
         self.pod._custom_preprocessor_value = self.config.value
