c1327050df8c630da80a75e4d69dfcac8818d3db
Jeremy Weinstein <jeremydw@gmail.com>
2016-03-30 15:27:36 -0700

    Implement CLEAR_AUTH and fix issue with logging output after completing oauth2 dance. Fixes #187. Fixes #189.

--- grow/common/oauth.py
+++ grow/common/oauth.py
@@ -1,6 +1,7 @@
 from oauth2client import client
 from oauth2client import keyring_storage
 from oauth2client import tools
+import logging
 import os
 
 # Google API details for a native/installed application for API project grow-prod.
@@ -12,6 +13,8 @@ DEFAULT_STORAGE_KEY = 'Grow SDK'
 
 
 def get_credentials(scope, storage_key=DEFAULT_STORAGE_KEY):
+    if os.getenv('CLEAR_AUTH'):
+        clear_credentials(storage_key=storage_key)
     username = os.getenv('AUTH_EMAIL_ADDRESS', 'default')
     storage = keyring_storage.Storage(storage_key, username)
     credentials = storage.get()
@@ -25,6 +28,8 @@ def get_credentials(scope, storage_key=DEFAULT_STORAGE_KEY):
         flow = client.OAuth2WebServerFlow(CLIENT_ID, CLIENT_SECRET, scope,
                                           redirect_uri=REDIRECT_URI)
         credentials = tools.run_flow(flow, storage, flags)
+        # run_flow changes the logging level, so change it back.
+        logging.getLogger().setLevel(getattr(logging, 'INFO'))
     return credentials
 
 
