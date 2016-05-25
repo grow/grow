b4ce725f3a7609f169c011b22f9e9e8c809073da
Stu Cox <stuart.cox@gmail.com>
2016-05-26 00:26:50 +0100

    Add test pods dedicated to testing many different contexts for localizable strings & update helpers to allow these pods to be used easily

--- grow/testing/testing.py
+++ grow/testing/testing.py
@@ -7,16 +7,17 @@ _here = os.path.dirname(__file__)
 TESTDATA_DIR = os.path.abspath(os.path.join(_here, 'testdata'))
 
 
-def create_test_pod_dir():
+# pod_dir is relative to testdata/ dir
+def create_test_pod_dir(pod_dir='pod'):
     dest_dir = tempfile.mkdtemp()
     dest_dir = os.path.join(dest_dir, 'test_pod')
-    source_dir = os.path.join(TESTDATA_DIR, 'pod')
+    source_dir = os.path.join(TESTDATA_DIR, pod_dir)
     shutil.copytree(source_dir, dest_dir)
     return dest_dir
 
 
-def create_test_pod():
-    return pods.Pod(create_test_pod_dir())
+def create_test_pod(pod_dir='pod'):
+    return pods.Pod(create_test_pod_dir(pod_dir))
 
 
 def get_testdata_dir():
