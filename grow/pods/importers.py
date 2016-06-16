2d9680147670d587122f1b98f2cdef8c640e0986
Jeremy Weinstein <jeremydw@gmail.com>
2016-06-16 10:27:27 -0400

    Fix issue when the import_as mapping for importing translations takes multiple values.

--- grow/pods/importers.py
+++ grow/pods/importers.py
@@ -1,5 +1,7 @@
+import collections
 from babel.messages import pofile
 import cStringIO
+import copy
 import errno
 import os
 import shutil
@@ -7,7 +9,8 @@ import tempfile
 import zipfile
 
 
-default_external_to_babel_locales = {
+default_external_to_babel_locales = collections.defaultdict(list)
+builtin_locales = {
     'en-GB': 'en_GB',
     'es-419': 'es_419',
     'fr-CA': 'fr_CA',
@@ -19,6 +22,8 @@ default_external_to_babel_locales = {
     'zh-HK': 'zh_Hant_HK',
     'zh-TW': 'zh_Hant_TW',
 }
+for key, value in builtin_locales.iteritems():
+    default_external_to_babel_locales[key].append(value)
 
 
 def _mkdir(path):
@@ -89,36 +94,41 @@ class Importer(object):
             raise Error('Must specify locale.')
 
         # Leverage user-defined locale identifiers when importing translations.
-        external_to_babel_locales = {}
-        external_to_babel_locales.update(default_external_to_babel_locales)
+        external_to_babel_locales = copy.deepcopy(
+            default_external_to_babel_locales)
         if self.pod.podspec.localization:
             if 'import_as' in self.pod.podspec.localization:
                 import_as = self.pod.podspec.localization['import_as']
                 for external_locale, babel_locales in import_as.iteritems():
                     for babel_locale in babel_locales:
-                        external_to_babel_locales[external_locale] = babel_locale
+                        external_to_babel_locales[external_locale].append(
+                            babel_locale)
 
-        babel_locale = external_to_babel_locales.get(locale, locale)
-        pod_translations_dir = os.path.join(
-            'translations', babel_locale, 'LC_MESSAGES')
-        pod_po_path = os.path.join(pod_translations_dir, 'messages.po')
-        if self.pod.file_exists(pod_po_path):
-            existing_po_file = self.pod.open_file(pod_po_path)
-            existing_catalog = pofile.read_po(existing_po_file, babel_locale)
-            po_file_to_merge = cStringIO.StringIO()
-            po_file_to_merge.write(content)
-            po_file_to_merge.seek(0)
-            catalog_to_merge = pofile.read_po(po_file_to_merge, babel_locale)
-            for message in catalog_to_merge:
-                if message.id not in existing_catalog:
-                    existing_catalog[message.id] = message
-                else:
-                    existing_catalog[message.id].string = message.string
-            existing_po_file = self.pod.open_file(pod_po_path, mode='w')
-            pofile.write_po(existing_po_file, existing_catalog, width=80,
-                            sort_output=True, sort_by_file=True)
-            text = 'Imported {} translations: {}'
-            self.pod.logger.info(text.format(len(catalog_to_merge), babel_locale))
-        else:
-            self.pod.write_file(pod_po_path, content)
-            self.pod.logger.info('Imported new catalog: {}'.format(babel_locale))
+        babel_locales = external_to_babel_locales.get(locale, [locale])
+        for babel_locale in babel_locales:
+          print 'babel locale', babel_locale
+          pod_translations_dir = os.path.join(
+              'translations', babel_locale, 'LC_MESSAGES')
+          pod_po_path = os.path.join(pod_translations_dir, 'messages.po')
+          if self.pod.file_exists(pod_po_path):
+              existing_po_file = self.pod.open_file(pod_po_path)
+              existing_catalog = pofile.read_po(existing_po_file, babel_locale)
+              po_file_to_merge = cStringIO.StringIO()
+              po_file_to_merge.write(content)
+              po_file_to_merge.seek(0)
+              catalog_to_merge = pofile.read_po(po_file_to_merge, babel_locale)
+              for message in catalog_to_merge:
+                  if message.id not in existing_catalog:
+                      existing_catalog[message.id] = message
+                  else:
+                      existing_catalog[message.id].string = message.string
+              existing_po_file = self.pod.open_file(pod_po_path, mode='w')
+              pofile.write_po(existing_po_file, existing_catalog, width=80,
+                              sort_output=True, sort_by_file=True)
+              text = 'Imported {} translations: {}'
+              message = text.format(len(catalog_to_merge), babel_locale)
+              self.pod.logger.info(message)
+          else:
+              self.pod.write_file(pod_po_path, content)
+              message = 'Imported new catalog: {}'.format(babel_locale)
+              self.pod.logger.info(message)
