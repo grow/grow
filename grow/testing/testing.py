10541e4827299b96b61c442455c64f3f1943f49e
Stu Cox <stuart.cox@gmail.com>
2016-06-17 21:08:20 +0100

    Rename `pod_dir` to `root`

--- grow/testing/testing.py
+++ grow/testing/testing.py
@@ -7,17 +7,17 @@ _here = os.path.dirname(__file__)
 TESTDATA_DIR = os.path.abspath(os.path.join(_here, 'testdata'))
 
 
-# pod_dir is relative to testdata/ dir
-def create_test_pod_dir(pod_dir='pod'):
+# root is relative to testdata/ dir
+def create_test_pod_dir(root='pod'):
     dest_dir = tempfile.mkdtemp()
     dest_dir = os.path.join(dest_dir, 'test_pod')
-    source_dir = os.path.join(TESTDATA_DIR, pod_dir)
+    source_dir = os.path.join(TESTDATA_DIR, root)
     shutil.copytree(source_dir, dest_dir)
     return dest_dir
 
 
-def create_test_pod(pod_dir='pod'):
-    return pods.Pod(create_test_pod_dir(pod_dir))
+def create_test_pod(root='pod'):
+    return pods.Pod(create_test_pod_dir(root))
 
 
 def get_testdata_dir():
