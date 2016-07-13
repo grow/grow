b2633ccf32bf50765116ccfbf200bf157953a92c
Jeremy Weinstein <jeremydw@gmail.com>
2016-07-13 11:34:29 -0700

    Simplify gulp setup.

--- grow/ui/gulpfile.js
+++ grow/ui/gulpfile.js
@@ -1,7 +1,13 @@
-var airkit = require('airkit/gulp/compilejs');
+var browserify = require('browserify');
+var buffer = require('vinyl-buffer');
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gutil = require('gulp-util');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var watchify = require('watchify');
+
 
 var Config = { 
   JS_SOURCES: [
@@ -14,17 +20,74 @@ var Config = {
   SASS_OUT_DIR: './dist/css/'
 };
 
+
+function rebundle_(bundler, outdir, outfile, opt_options) {
+  var options = opt_options || {};
+  return bundler.bundle()
+    .on('error', gutil.log.bind(gutil, 'browserify error'))
+    .pipe(source(outfile))
+    .pipe(buffer())
+    .pipe(uglify(options.uglify))
+    .pipe(gulp.dest(outdir));
+}
+
+
+/**
+ * Compiles js code using browserify and uglify.
+ * @param {Array.<string>} sources List of JS source files.
+ * @param {string} outdir Output directory.
+ * @param {string} outfile Output file name.
+ * @param {Object=} opt_options Options.
+ */
+function compilejs(sources, outdir, outfile, opt_options) {
+  var bundler = browserify({
+    entries: sources,
+    debug: false
+  });
+  return rebundle_(bundler, outdir, outfile, opt_options);
+}
+
+
+/**
+ * Watches JS code for changes and triggers compilation.
+ * @param {Array.<string>} sources List of JS source files.
+ * @param {string} outdir Output directory.
+ * @param {string} outfile Output file name.
+ * @param {Object=} opt_options Options.
+ */
+function watchjs(sources, outdir, outfile, opt_options) {
+  var bundler = watchify(browserify({
+    entries: sources,
+    debug: false,
+    // Watchify options:
+    cache: {},
+    packageCache: {},
+    fullPaths: true
+  }));
+
+  bundler.on('update', function() {
+    gutil.log('recompiling js...');
+    rebundle_(bundler, outdir, outfile);
+    gutil.log('finished recompiling js');
+  });
+  return rebundle_(bundler, outdir, outfile, opt_options);
+}
+
+
 gulp.task('compilejs', function() {
-    return airkit.compilejs(Config.JS_SOURCES, Config.JS_OUT_DIR, Config.JS_OUT_FILE);
+    return compilejs(Config.JS_SOURCES, Config.JS_OUT_DIR, Config.JS_OUT_FILE);
 });
 
+
 gulp.task('watchjs', function() {
-    return airkit.watchjs(Config.JS_SOURCES, Config.JS_OUT_DIR, Config.JS_OUT_FILE);
+    return watchjs(Config.JS_SOURCES, Config.JS_OUT_DIR, Config.JS_OUT_FILE);
 });
+
 
 gulp.task('watchcss', function() {
   return gulp.watch(Config.SASS_SOURCE_FORMAT, ['compilecss']);
 });
+
 
 gulp.task('compilecss', function() {
   gulp.src(Config.SASS_SOURCE)
@@ -36,6 +99,7 @@ gulp.task('compilecss', function() {
     }))
     .pipe(gulp.dest(Config.SASS_OUT_DIR));
 });
+
 
 gulp.task('build', ['compilejs', 'compilecss']);
 gulp.task('default', ['build', 'watchjs', 'watchcss']);
