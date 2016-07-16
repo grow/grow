dcd81919ea098d99b164d2aa1a9b1cce586c94bb
Jeremy Weinstein <jeremydw@gmail.com>
2016-07-16 10:16:57 -0700

    Allow :enabled = False on the document-level to disable inclusion from sitemaps.

--- grow/pods/sitemap_test.py
+++ grow/pods/sitemap_test.py
@@ -15,6 +15,32 @@ class SitemapTest(unittest.TestCase):
         controller = sitemap.SitemapController(pod=self.pod)
         controller.render()
 
+    def test_sitemap_enabled(self):
+        pod = testing.create_pod()
+        pod.write_yaml('/podspec.yaml', {
+            'sitemap': {
+                'enabled': True,
+            },
+        })
+        pod.write_yaml('/content/pages/_blueprint.yaml', {
+            '$path': '/{base}/',
+            '$view': '/views/base.html',
+        })
+        pod.write_yaml('/content/pages/foo.yaml', {
+            '$title': 'Foo',
+        })
+        pod.write_yaml('/content/pages/bar.yaml', {
+            '$title': 'Bar',
+            '$sitemap': {
+                'enabled': False,
+            },
+        })
+        controller, params = pod.match('/sitemap.xml')
+        content = controller.render(params)
+        # Verify $sitemap:enabled = false.
+        self.assertIn('/foo/', content)
+        self.assertNotIn('/bar/', content)
+
 
 if __name__ == '__main__':
     unittest.main()
