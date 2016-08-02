0281cbbe4d512f568cee88f77716a1c8ddf6a615
Jeremy Weinstein <jeremydw@gmail.com>
2016-08-02 09:17:32 -0700

    Use remap instead of walk for translation untagging. #142

--- grow/common/utils_test.py
+++ grow/common/utils_test.py
@@ -1,9 +1,10 @@
 from grow.testing import testing
 from grow.common.sdk_utils import get_this_version, LatestVersionCheckError
 from . import utils
-import unittest
-import semantic_version
+import copy
 import mock
+import semantic_version
+import unittest
 
 
 class UtilsTestCase(unittest.TestCase):
@@ -45,6 +46,59 @@ class UtilsTestCase(unittest.TestCase):
             mock_version.return_value = gt_version
             with self.assertRaises(LatestVersionCheckError):
                 pod = testing.create_test_pod()
+
+    def test_untag_fields(self):
+        fields_to_test = {
+            'title': 'value-none',
+            'title@fr': 'value-fr',
+            'list': [
+                {
+                    'list-item-title': 'value-none',
+                    'list-item-title@fr': 'value-fr',
+                },
+            ],
+            'sub-nested': {
+                'sub-nested': {
+                    'nested@': 'sub-sub-nested-value',
+                },
+            },
+            'nested': {
+                'nested-none': 'nested-value-none',
+                'nested-title@': 'nested-value-none',
+            },
+            'nested@fr': {
+                'nested-title@': 'nested-value-fr',
+            },
+            'list@de': [
+                'list-item-de',
+            ]
+        }
+        fields = copy.deepcopy(fields_to_test)
+        self.assertDictEqual({
+            'title': 'value-fr',
+            'list': [{'list-item-title': 'value-fr'},],
+            'nested': {'nested-title': 'nested-value-fr',},
+            'sub-nested': {
+                'sub-nested': {
+                    'nested': 'sub-sub-nested-value',
+                },
+            },
+        }, utils.untag_fields(fields, locale='fr'))
+
+        fields = copy.deepcopy(fields_to_test)
+        self.assertDictEqual({
+            'title': 'value-none',
+            'list': ['list-item-de',],
+            'nested': {
+                'nested-none': 'nested-value-none',
+                'nested-title': 'nested-value-none',
+            },
+            'sub-nested': {
+                'sub-nested': {
+                    'nested': 'sub-sub-nested-value',
+                },
+            },
+        }, utils.untag_fields(fields, locale='de'))
 
 
 if __name__ == '__main__':
