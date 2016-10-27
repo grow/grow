92e3f15b86e5b79117365b31535a319dd0a5f6c9
Randy Merrill <randy@blinkk.com>
2016-10-27 13:04:59 -0700

    Updating the styling and display of the grow menu.
    Added a main 'Grow' icon that acts as the master grouping.
    
    On hover the other menu options will appear.
    
    On click of the main 'Grow' icon the menu will expand and stay until clicked again. This helps it function on mobile environments where there is no hover.

--- grow/ui/js/ui.js
+++ grow/ui/js/ui.js
@@ -2,54 +2,74 @@ var grow = grow || {};
 grow.ui = grow.ui || {};
 window.grow = grow;
 
-grow.ui.main = function(settings) {
-  var buttonEl;
-  var buttonsEl = document.createElement('div');
-  buttonsEl.setAttribute('class', 'buttons');
-  buttonsEl.addEventListener('mouseover', function() {
-    buttonsEl.classList.add('expand');
-  });
-  buttonsEl.addEventListener('mouseout', function() {
-    buttonsEl.classList.remove('expand');
-  });
+(function(grow){
+  grow.ui.main = function(settings) {
+    var el = document.createElement('div');
+    var buttonsEl = document.createElement('div');
+    buttonsEl.setAttribute('class', 'grow__buttons');
 
-  if (settings.injectEditUrl) {
-    buttonsEl.appendChild(
-      grow.ui.main.createButton_('edit', settings.injectEditUrl));
-  }
+    buttonsEl.appendChild(createButton_('primary', null, null, {
+      'click': function() {
+        buttonsEl.classList.toggle('grow__expand');
+      }
+    }));
 
-  if (settings.injectTranslateUrl) {
-    buttonsEl.appendChild(grow.ui.main.createButton_(
-      'translate', settings.injectTranslateUrl));
-  }
+    buttonsEl.appendChild(createButton_('close', null, null, {
+      'click': function() {
+        el.parentNode.removeChild(el);
+      }
+    }));
 
-  buttonsEl.appendChild(grow.ui.main.createButton_('primary', null, {
-    'click': function() {
-      buttonsEl.classList.toggle('expand');
+    if (settings.injectEditUrl) {
+      buttonsEl.appendChild(
+        createButton_('edit', 'Edit', settings.injectEditUrl));
     }
-  }));
 
-  var el = document.createElement('div');
-  el.setAttribute('id', 'grow-utils');
-  el.appendChild(buttonsEl);
-  document.body.appendChild(el);
-};
-
-grow.ui.main.createButton_ = function(kind, url, events) {
-  var buttonEl = document.createElement('a');
-  buttonEl.classList.add('button');
-  buttonEl.classList.add('icon_' + kind);
-
-  if (url) {
-    buttonEl.setAttribute('href', url);
-    buttonEl.setAttribute('target', '_blank');
-  }
-
-  if (events) {
-    for (var prop in events) {
-      buttonEl.addEventListener(prop, events[prop]);
+    if (settings.injectTranslateUrl) {
+      buttonsEl.appendChild(createButton_(
+        'translate', 'Translate', settings.injectTranslateUrl));
     }
-  }
 
-  return buttonEl;
-};
+    // If there is nothing to do, do not show the menu at all.
+    if (buttonsEl.children.length <= 2) {
+      return;
+    }
+
+    el.setAttribute('id', 'grow-utils');
+    el.appendChild(buttonsEl);
+    document.body.appendChild(el);
+  };
+
+  var createButton_ = function(kind, label, url, events) {
+    var containerEl = document.createElement('div');
+    containerEl.classList.add('grow__button');
+
+    var buttonEl = document.createElement('a');
+    buttonEl.classList.add('grow__icon');
+    buttonEl.classList.add('grow__icon_' + kind);
+
+    if (url) {
+      buttonEl.setAttribute('href', url);
+      buttonEl.setAttribute('target', '_blank');
+    } else {
+      buttonEl.setAttribute('href', 'javascript:void(0)');
+    }
+
+    if (events) {
+      for (var prop in events) {
+        buttonEl.addEventListener(prop, events[prop]);
+      }
+    }
+
+    containerEl.appendChild(buttonEl);
+
+    if (label) {
+      var labelEl = document.createElement('p');
+      labelEl.appendChild(document.createTextNode(label));
+      labelEl.classList.add('grow__label');
+      containerEl.appendChild(labelEl);
+    }
+
+    return containerEl;
+  };
+})(grow);
