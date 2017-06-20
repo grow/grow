fd875d15ec1d76ff12408166cde8671fe587988d
Charlie Morgan <chargrizzled@gmail.com>
2017-06-20 12:09:57 -0700

    Add manifest_path to args.

--- grow/commands/generate_manifest.py
+++ grow/commands/generate_manifest.py
@@ -7,13 +7,14 @@ import json
 
 @click.command()
 @click.argument('statics_path')
+@click.argument('manifest_path', default="manifest.json")
 @click.argument('pod_path', default='.')
-def generate_manifest(statics_path, pod_path):
+def generate_manifest(statics_path, manifest_path, pod_path):
     """Generates a g.static manifiest given a pod path."""
     root = os.path.abspath(os.path.join(os.getcwd(), pod_path))
     pod = pods.Pod(root, storage=storage.FileStorage)
     try:
-        with open('manifest.json', 'w') as f:
+        with open(manifest_path, 'w') as f:
             data = {static.pod_path: static.url.path for static in pod.list_statics(statics_path)}
             f.write(json.dumps(data))
     except pods.Error as e:
