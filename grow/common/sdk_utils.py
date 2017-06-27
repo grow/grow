afad7fb090f58a146b09c1d92a046b232a1b9e1c
Jeremy Weinstein <jeremydw@gmail.com>
2017-06-26 20:19:06 -0700

    Make Grow-managed extensions directory user-customizable.

--- grow/common/sdk_utils.py
+++ grow/common/sdk_utils.py
@@ -14,7 +14,6 @@ from grow.common import utils
 from xtermcolor import colorize
 
 
-EXTENSIONS_DIR_NAME = 'extensions'
 VERSION = config.VERSION
 RELEASES_API = 'https://api.github.com/repos/grow/grow/releases'
 INSTALLER_COMMAND = ('/usr/bin/python -c "$(curl -fsSL '
@@ -216,7 +215,8 @@ def install_bower(pod):
     if bower_not_found:
         pod.logger.error('[✘] The "bower" command was not found.')
         pod.logger.error(
-            '    Either add bower to package.json or install globally using: sudo npm install -g bower')
+            '    Either add bower to package.json or install globally using:'
+            ' sudo npm install -g bower')
         return
     pod.logger.info('[✓] "bower" is installed.')
     bower_command = 'bower install'
@@ -236,7 +236,8 @@ def install_gulp(pod):
     if gulp_not_found:
         pod.logger.error('[✘] The "gulp" command was not found.')
         pod.logger.error(
-            '    Either add gulp to package.json or install globally using: sudo npm install -g gulp')
+            '    Either add gulp to package.json or install globally using:'
+            ' sudo npm install -g gulp')
         return
     pod.logger.info('[✓] "gulp" is installed.')
     return True
@@ -250,18 +251,20 @@ def install_extensions(pod):
     if pip_not_found:
         pod.logger.error('[✘] The "pip" command was not found.')
         return
+    extensions_dir = pod.extensions_dir
     pod.logger.info('[✓] "pip" is installed.')
-    pip_command = 'pip install -U -t {} -r extensions.txt'.format(EXTENSIONS_DIR_NAME)
+    command = 'pip install -U -t {} -r extensions.txt'
+    pip_command = command.format(extensions_dir)
     process = subprocess.Popen(pip_command, shell=True, **args)
     code = process.wait()
     if not code:
-        init_file_name = '/{}/__init__.py'.format(EXTENSIONS_DIR_NAME)
+        init_file_name = '/{}/__init__.py'.format(extensions_dir)
         if not pod.file_exists(init_file_name):
             pod.write_file(init_file_name, '')
-        pod.logger.info('[✓] Installed: Grow extensions from extensions.txt.')
+        text = '[✓] Installed: extensions.txt -> {}'
+        pod.logger.info(text.format(extensions_dir))
         return True
-    pod.logger.error(
-        '[✘] There was an error running "pip install -t ext -r extensions.txt".')
+    pod.logger.error('[✘] There was an error running "{}".'.format(pip_command))
 
 
 def install_yarn(pod):
