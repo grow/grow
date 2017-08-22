4bf525c12642cb6a924e393965577588673f5d3b
Randy Merrill <randy@blinkk.com>
2017-08-22 13:51:08 -0600

    Tweaking format.

--- grow/pods/pods.py
+++ grow/pods/pods.py
@@ -276,8 +276,8 @@ class Pod(object):
         output = {}
         text = 'Building: %(value)d/{} (in %(seconds_elapsed)s)'
         widgets = [progressbar.FormatLabel(text.format(len(paths)))]
-        bar = progressbar_non.create_progressbar("Building pod...",
-            widgets=widgets, max_value=len(paths))
+        bar = progressbar_non.create_progressbar(
+            "Building pod...", widgets=widgets, max_value=len(paths))
         bar.start()
         for path in paths:
             output_path = path
@@ -334,8 +334,8 @@ class Pod(object):
 
         text = 'Building UI Tools: %(value)d/{} (in %(seconds_elapsed)s)'
         widgets = [progressbar.FormatLabel(text.format(len(paths)))]
-        progress = progressbar_non.create_progressbar("Building UI Tools...",
-            widgets=widgets, max_value=len(paths))
+        progress = progressbar_non.create_progressbar(
+            "Building UI Tools...", widgets=widgets, max_value=len(paths))
         progress.start()
         for path in paths:
             output_path = path.replace(
