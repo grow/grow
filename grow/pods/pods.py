30077bd7f64c0dd566ca4422f5e3d46376927df5
Randy Merrill <randy@blinkk.com>
2017-09-20 09:15:49 -0600

    Adding profiling around the file operations for reading and writing.

--- grow/pods/pods.py
+++ grow/pods/pods.py
@@ -660,7 +660,8 @@ class Pod(object):
             for trigger_doc in trigger_docs:
                 if trigger_doc.has_serving_path():
                     try:
-                        _ = self.routes.match(trigger_doc.get_serving_path(), env=route_env)
+                        _ = self.routes.match(
+                            trigger_doc.get_serving_path(), env=route_env)
                     except webob_exc.HTTPNotFound:
                         added_docs.append(trigger_doc)
             if added_docs or removed_docs:
@@ -688,24 +689,27 @@ class Pod(object):
                 time.sleep(ratelimit)
 
     def read_csv(self, path, locale=utils.SENTINEL):
-        return utils.get_rows_from_csv(pod=self, path=path, locale=locale)
+        with self.profile.timer('pod.read_csv', label=path, meta={'path': path}):
+            return utils.get_rows_from_csv(pod=self, path=path, locale=locale)
 
     def read_file(self, pod_path):
         path = self._normalize_path(pod_path)
-        return self.storage.read(path)
+        with self.profile.timer('pod.read_file', label=path, meta={'path': path}):
+            return self.storage.read(path)
 
     def read_json(self, path):
         fp = self.open_file(path)
         return json.load(fp)
 
     def read_yaml(self, path, locale=None):
-        fields = utils.parse_yaml(self.read_file(path), pod=self)
-        try:
-            untag = document_fields.DocumentFields.untag
-            return untag(fields, env_name=self.env.name, locale=locale)
-        except Exception as e:
-            logging.error('Error parsing -> {}'.format(path))
-            raise
+        with self.profile.timer('pod.read_yaml', label=path, meta={'path': path}):
+            fields = utils.parse_yaml(self.read_file(path), pod=self)
+            try:
+                return document_fields.DocumentFields.untag(
+                    fields, env_name=self.env.name, locale=locale)
+            except Exception as e:
+                logging.error('Error parsing -> {}'.format(path))
+                raise
 
     def render_paths(self, paths, routes, suffix=None, append_slashes=False):
         """Renders the given paths and yields each path and content."""
@@ -759,11 +763,15 @@ class Pod(object):
         return self.storage.walk(path)
 
     def write_file(self, pod_path, content):
-        path = self._normalize_path(pod_path)
-        self.storage.write(path, content)
+        with self.profile.timer(
+                'pod.write_file', label=pod_path, meta={'path': pod_path}):
+            path = self._normalize_path(pod_path)
+            self.storage.write(path, content)
 
     def write_yaml(self, path, content):
-        self.podcache.collection_cache.remove_by_path(path)
-        self.podcache.document_cache.remove_by_path(path)
-        content = utils.dump_yaml(content)
-        self.write_file(path, content)
+        with self.profile.timer(
+                'pod.write_yaml', label=path, meta={'path': path}):
+            self.podcache.collection_cache.remove_by_path(path)
+            self.podcache.document_cache.remove_by_path(path)
+            content = utils.dump_yaml(content)
+            self.write_file(path, content)
