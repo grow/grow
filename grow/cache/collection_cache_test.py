b213c471d4b8e27749fdce1567f801ecf358799d
Randy Merrill <randy@blinkk.com>
2017-09-20 10:35:15 -0600

    Updating test references.

--- grow/cache/collection_cache_test.py
+++ grow/cache/collection_cache_test.py
@@ -1,8 +1,10 @@
-from . import collection_cache
-from . import pods
-from . import storage
-from grow.testing import testing
+"""Tests for the collection cache."""
+
 import unittest
+from grow.pods import pods
+from grow.pods import storage
+from grow.testing import testing
+from . import collection_cache
 
 
 class DocumentCacheTestCase(unittest.TestCase):
