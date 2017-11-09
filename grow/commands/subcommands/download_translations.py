aed7e207fc46a5a2fc22fd9909ab8a0b32feb792
Jeremy Weinstein <jeremydw@gmail.com>
2017-11-09 13:33:09 -0800

    Add --include-obsolete flag to download_translations command. The idea is we should be able to download translations from Trix without including translations that might be obsolete/no longer used in the build. The translations can continue to live in Google Sheets if they're needed in the future (and can be removed with the --prune flag), but downloading with --no-include-obsolete should be the best way to download only what's needed.

--- grow/commands/subcommands/download_translations.py
+++ grow/commands/subcommands/download_translations.py
@@ -16,11 +16,12 @@ CFG = rc_config.RC_CONFIG.prefixed('grow.download_translations')
 @shared.locale_option(help_text='Which locale(s) to download. If unspecified,'
                                 ' translations for all locales will be downloaded.')
 @shared.service_option
-def download_translations(pod_path, locale, service):
+@shared.include_obsolete_option(CFG, default_value=True)
+def download_translations(pod_path, locale, service, include_obsolete):
     """Downloads translations from a translation service."""
     root = os.path.abspath(os.path.join(os.getcwd(), pod_path))
     pod = pods.Pod(root, storage=storage.FileStorage)
     with pod.profile.timer('grow_download_translations'):
         translator = pod.get_translator(service)
-        translator.download(locales=locale)
+        translator.download(locales=locale, include_obsolete=include_obsolete)
     return pod
