cce70f6e10890b01382698a8530a67bef3255f2b
Randy Merrill <randy@blinkk.com>
2017-11-09 17:13:00 -0700

    Fixing the test for the updated order.

--- grow/pods/collection_test.py
+++ grow/pods/collection_test.py
@@ -86,8 +86,8 @@ class CollectionsTestCase(unittest.TestCase):
         expected_docs = [
             self.pod.get_doc(pod_path, locale='fr')
             for pod_path in [
-                '/content/pages/yaml_test.html',
                 '/content/pages/json_test.html',
+                '/content/pages/yaml_test.html',
                 '/content/pages/home.yaml',
                 '/content/pages/intro@fr.md',
                 '/content/pages/about.yaml',
