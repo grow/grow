0a34b40294741c55c1bbd4be192c05f8fa290dea
Randy Merrill <randy@blinkk.com>
2017-11-17 11:55:25 -0700

    Improving coverage for structures test.

--- grow/common/structures_test.py
+++ grow/common/structures_test.py
@@ -29,6 +29,24 @@ class SortedCollectionTestCase(unittest.TestCase):
                 ('david', 'thomas', 32)]:
             self.coll.insert(record)
 
+    def test_contains(self):
+        """Contains matches."""
+        self.assertTrue(('roger', 'young', 30) in self.coll)
+        self.assertFalse(('bob', 'young', 30) in self.coll)
+
+    def test_count(self):
+        """Counts matches."""
+        self.assertEqual(1, self.coll.count(('roger', 'young', 30)))
+        self.coll.insert(('roger', 'young', 30))
+        self.assertEqual(2, self.coll.count(('roger', 'young', 30)))
+
+    def test_find(self):
+        """Find first match."""
+        self.assertEqual(('angela', 'jones', 28), self.coll.find(28))
+
+        with self.assertRaises(ValueError):
+            self.coll.find(39)
+
     def test_ge(self):
         """Greater than equal."""
         self.assertEqual(('angela', 'jones', 28), self.coll.find_ge(28))
@@ -69,6 +87,13 @@ class SortedCollectionTestCase(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             self.coll.find_lt(10)
+
+    def test_remove(self):
+        """Removes matches."""
+        item = ('roger', 'young', 30)
+        self.assertTrue(item in self.coll)
+        self.coll.remove(item)
+        self.assertFalse(item in self.coll)
 
     def test_sorting(self):
         """Collection is sorted."""
