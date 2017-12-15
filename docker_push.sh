937096380a5eef49b0dab32267725e30f807aa57
Randy Merrill <randy@blinkk.com>
2017-12-15 16:25:00 -0700

    Fixing the docker image to support the old name for the baseimage.

--- docker_push.sh
+++ docker_push.sh
@@ -6,15 +6,22 @@ GROW_VERSION=`cat grow/VERSION`
 
 echo "Building and Pushing Grow $GROW_VERSION to Docker Hub"
 
-docker build --no-cache --build-arg grow_version=$GROW_VERSION -t grow/base:$GROW_VERSION -t grow/base:latest .
+docker build --no-cache --build-arg grow_version=$GROW_VERSION \
+  -t grow/base:$GROW_VERSION -t grow/base:latest \
+  -t grow/baseimage:$GROW_VERSION -t grow/baseimage:latest .
 
 docker run --rm=true --workdir=/tmp -i grow/base:$GROW_VERSION  \
   bash -c "git clone https://github.com/grow/grow.io.git && grow install && grow build grow.io/"
 
 docker push grow/base:$GROW_VERSION
 docker push grow/base:latest
+# Old docker image support.
+docker push grow/baseimage:$GROW_VERSION
+docker push grow/baseimage:latest
 
-docker build --no-cache --build-arg grow_version=$GROW_VERSION -t grow/grow:$GROW_VERSION -t grow/grow:latest -f Dockerfile.exec .
+docker build --no-cache --build-arg grow_version=$GROW_VERSION \
+  -t grow/grow:$GROW_VERSION -t grow/grow:latest \
+  -f Dockerfile.exec .
 
 docker run grow/grow:$GROW_VERSION --version
 
