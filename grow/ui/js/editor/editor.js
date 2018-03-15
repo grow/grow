d2aed2582a5c3b5ee1e959afb856fcce6eed6a0c
Randy Merrill <randy@blinkk.com>
2018-03-15 11:25:30 -0600

    Making the mobile toggle work.

--- grow/ui/js/editor/editor.js
+++ grow/ui/js/editor/editor.js
@@ -5,5 +5,13 @@
 export default class Editor {
   constructor(containerEl) {
     this.containerEl = containerEl
+    this.mobileToggleEl = this.containerEl.querySelector('.content__device')
+    this.contentPreviewEl = this.containerEl.querySelector('.content__preview')
+
+    this.mobileToggleEl.addEventListener('click', this.handleMobileClick.bind(this))
+  }
+
+  handleMobileClick() {
+    this.contentPreviewEl.classList.toggle('content__preview--mobile')
   }
 }
