5019b89d9f11879bde67bbf9cc7035c3b35187a3
Randy Merrill <randy@blinkk.com>
2018-03-15 10:14:11 -0600

    Fixing the JS test for ui js.

--- grow/server/main_test.py
+++ grow/server/main_test.py
@@ -71,7 +71,7 @@ class PodHandlerTestCase(unittest.TestCase):
         request = webapp2.Request.blank('/_grow/ui/js/ui.min.js')
         response = request.get_response(app)
         self.assertEqual(200, response.status_int)
-        js_sentinel = '!function'
+        js_sentinel = 'function(modules)'
         self.assertIn(js_sentinel, response.body)
 
         request = webapp2.Request.blank('/_grow/ui/css/ui.min.css')
