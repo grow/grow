7f2210cefe628188fe86d3681eb5123700cb86e9
Randy Merrill <randy@blinkk.com>
2018-04-17 12:04:33 -0600

    Mimetype of serving path trumps the view mimetype.

--- grow/pods/rendered.py
+++ grow/pods/rendered.py
@@ -37,7 +37,8 @@ class RenderedController(controllers.BaseController):
             self.view, doc.pod_path)
 
     def get_mimetype(self, params=None):
-        return mimetypes.guess_type(self.view)[0]
+        path_mimetype = mimetypes.guess_type(self.doc.get_serving_path())[0]
+        return path_mimetype or mimetypes.guess_type(self.view)[0]
 
     @property
     def doc(self):
