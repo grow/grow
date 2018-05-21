ad19ac57c65dcfeb8862c456ce30c88abd72cc18
Randy Merrill <randy@blinkk.com>
2018-05-21 12:54:19 -0600

    Adding a message when the package.json file changes.
    Fixes #762

---
+++ grow/extensions/core/node_extension_test.py
@@ -0,0 +1,12 @@
+"""Tests for node extension."""
+
+import unittest
+from grow.extensions.core import node_extension
+
+
+class NodeExtensionTestCase(unittest.TestCase):
+    """Test the node extension."""
+
+    def test_something(self):
+        """?"""
+        pass
