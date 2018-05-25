1b96d516287992f832a92baac501b3d0bc10b264
Randy Merrill <randy@blinkk.com>
2018-05-25 10:17:04 -0600

    Adding a dummy test file for the preprocess hook.

---
+++ grow/extensions/hooks/preprocess_hook_test.py
@@ -0,0 +1,12 @@
+"""Tests for preprocess hook."""
+
+import unittest
+from grow.extensions.hooks import preprocess_hook
+
+
+class PreprocessHookTestCase(unittest.TestCase):
+    """Test the preprocess hook."""
+
+    def test_something(self):
+        """?"""
+        pass
