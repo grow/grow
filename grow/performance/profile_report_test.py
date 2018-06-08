2b94b0f7b6aa41effd979706b1d11d47b0758bc7
Randy Merrill <randy@blinkk.com>
2018-06-08 11:43:26 -0600

    Adding some more tests for the profile reporting.

--- grow/performance/profile_report_test.py
+++ grow/performance/profile_report_test.py
@@ -10,11 +10,108 @@ class TimerReportTestCase(unittest.TestCase):
 
     def setUp(self):
         self.profile = profile.Profile()
-        self.report = profile_report.ProfileReport(self.profile)
 
     def test_analyze_empty(self):
-        """Test report."""
-        self.assertEqual(None, self.report.analyze())
+        """Test empty report."""
+        report = profile_report.ProfileReport(self.profile)
+        self.assertEqual({}, report.export())
+
+    def test_single_timer(self):
+        """Test report with timer."""
+        timer = profile.Timer('a')
+        timer.start = 1
+        timer.end = 3
+        self.profile.add_timer(timer)
+        report = profile_report.ProfileReport(self.profile)
+        self.assertEqual({
+            'a': {
+                'end': 3,
+                'start': 1,
+                'timers': [
+                    {
+                        'end': 3,
+                        'key': 'a',
+                        'label': 'a',
+                        'meta': None,
+                        'start': 1
+                    },
+                ]
+            },
+        }, report.export())
+
+    def test_grouped_timers(self):
+        """Test report with similar timer keys."""
+        timer = profile.Timer('a')
+        timer.start = 1
+        timer.end = 3
+        self.profile.add_timer(timer)
+        timer = profile.Timer('a')
+        timer.start = 2
+        timer.end = 5
+        self.profile.add_timer(timer)
+        report = profile_report.ProfileReport(self.profile)
+        self.assertEqual({
+            'a': {
+                'end': 5,
+                'start': 1,
+                'timers': [
+                    {
+                        'end': 3,
+                        'key': 'a',
+                        'label': 'a',
+                        'meta': None,
+                        'start': 1
+                    },
+                    {
+                        'end': 5,
+                        'key': 'a',
+                        'label': 'a',
+                        'meta': None,
+                        'start': 2
+                    },
+                ]
+            },
+        }, report.export())
+
+    def test_multiple_timers(self):
+        """Test report with similar timer keys."""
+        timer = profile.Timer('a')
+        timer.start = 1
+        timer.end = 3
+        self.profile.add_timer(timer)
+        timer = profile.Timer('b')
+        timer.start = 2
+        timer.end = 5
+        self.profile.add_timer(timer)
+        report = profile_report.ProfileReport(self.profile)
+        self.assertEqual({
+            'a': {
+                'end': 3,
+                'start': 1,
+                'timers': [
+                    {
+                        'end': 3,
+                        'key': 'a',
+                        'label': 'a',
+                        'meta': None,
+                        'start': 1
+                    },
+                ]
+            },
+            'b': {
+                'end': 5,
+                'start': 2,
+                'timers': [
+                    {
+                        'end': 5,
+                        'key': 'b',
+                        'label': 'b',
+                        'meta': None,
+                        'start': 2
+                    },
+                ]
+            },
+        }, report.export())
 
 
 if __name__ == '__main__':
