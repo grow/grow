48f7dea014c742dc3bdebf76b94e369026c1a610
Randy Merrill <randy@blinkk.com>
2018-06-18 14:50:42 -0600

    Adding the tests for the hook triggering for pre-render.

--- grow/extensions/hooks/preprocess_hook_test.py
+++ grow/extensions/hooks/preprocess_hook_test.py
@@ -7,6 +7,39 @@ from grow.extensions.hooks import preprocess_hook
 class PreprocessHookTestCase(unittest.TestCase):
     """Test the preprocess hook."""
 
-    def test_something(self):
-        """?"""
-        pass
+    def setUp(self):
+        self.hook = preprocess_hook.PreprocessHook(None)
+
+    def test_parse_config(self):
+        """Correctly parses the config object."""
+        self.assertEqual(self.hook.Config(), self.hook.parse_config({}))
+
+    def test_should_trigger_autorun(self):
+        """Should not trigger when not the autorun."""
+        self.assertFalse(self.hook.should_trigger(
+            None, {'autorun': False}, None, None, None, None))
+
+    def test_should_trigger_kind(self):
+        """Should not trigger when not the same kind."""
+        self.assertFalse(self.hook.should_trigger(
+            None, {'kind': 'Something'}, None, None, None, None))
+
+    def test_should_trigger_name(self):
+        """Should not trigger when not the same kind."""
+        self.assertTrue(self.hook.should_trigger(
+            None, {'name': 'test'}, ['test'], None, None, None))
+
+    def test_should_trigger_tags(self):
+        """Should not trigger when not the same kind."""
+        self.assertTrue(self.hook.should_trigger(
+            None, {'tags': ['test']}, None, ['test'], None, None))
+
+    def test_trigger(self):
+        """Base hook triggers with no-op."""
+        self.assertEqual(
+            None, self.hook.trigger(None, {}, None, None, None, None))
+
+    def test_trigger_previous_result(self):
+        """Base hook triggers with no-op to previous result."""
+        self.assertEqual(
+            'Testing', self.hook.trigger('Testing', {}, None, None, None, None))
