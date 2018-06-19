031a4e153065a0ad1c460634a819d4322aa05fe8
Randy Merrill <randy@blinkk.com>
2018-06-19 11:32:51 -0600

    Fixing the imports for the extension hooks.

--- grow/extensions/hooks/__init__.py
+++ grow/extensions/hooks/__init__.py
@@ -1,9 +1,6 @@
 """Extension hooks."""
 
 from . import dev_file_change_hook
-from . import dev_handler_hook
-from . import post_render_hook
-from . import pre_render_hook
 from . import preprocess_hook
 
 # pylint: disable=invalid-name
