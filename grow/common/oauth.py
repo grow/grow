9a03e88c68a0795e2ae367f8b2ebc2c6dc2343f5
Randy Merrill <randy@blinkk.com>
2018-08-03 12:58:50 -0600

    Updating to use the file based credential storage.

--- grow/common/oauth.py
+++ grow/common/oauth.py
@@ -2,12 +2,8 @@
 
 import os
 
-# Silence "Loading" messages from keyring.
-import logging
-log = logging.getLogger('keyring.backend')
-log.setLevel(logging.WARNING)
-
 from oauth2client import client
+from oauth2client import file as oauth_file
 from oauth2client import service_account
 from oauth2client import tools
 
@@ -34,8 +30,11 @@ def get_storage(key, username):
     if appengine:
         return appengine.StorageByKeyName(
             appengine.CredentialsModel, username, 'credentials')
-    from oauth2client.contrib import keyring_storage
-    return keyring_storage.Storage(key, username)
+    file_name = os.path.expanduser('~/.config/grow/{}'.format(username))
+    dir_name = os.path.dirname(file_name)
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+    return oauth_file.Storage(file_name)
 
 
 def get_credentials_and_storage(scope, storage_key=DEFAULT_STORAGE_KEY):
