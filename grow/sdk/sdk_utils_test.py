1688b1fe9c1b73d9a2c5c0f4b6bc035997a7b891
Randy Merrill <randy@blinkk.com>
2018-08-14 10:21:21 -0600

    Fixing the sdk utils tests.

--- grow/sdk/sdk_utils_test.py
+++ grow/sdk/sdk_utils_test.py
@@ -3,11 +3,8 @@
 import os
 import unittest
 from grow.common import base_config
-from grow.pods import env as environment
-from grow.pods import pods
-from grow import storage
 from grow.sdk import sdk_utils
-from grow.testing import testing
+from grow.testing import mocks
 
 
 class SdkUtilsTestCase(unittest.TestCase):
@@ -15,21 +12,25 @@ class SdkUtilsTestCase(unittest.TestCase):
 
     def setUp(self):
         self.config = base_config.BaseConfig()
-        self.dir_path = testing.create_test_pod_dir()
-        self.pod = pods.Pod(self.dir_path, storage=storage.FileStorage)
+        env = mocks.mock_env(name="testing")
+        self.pod = mocks.mock_pod(env=env, root='/testing/')
 
     def test_subprocess_args(self):
         """Test subprocess args."""
         args = sdk_utils.subprocess_args(self.pod)
         ending = os.path.join('node_modules', '.bin')
         self.assertTrue(args['env']['PATH'].endswith(ending))
-        self.assertFalse('GROW_ENVIRONMENT_NAME' in args['env'])
 
     def test_subprocess_args_env_name(self):
         """Test subprocess args env name."""
-        self.pod.set_env(environment.Env(environment.EnvConfig(name='testing')))
         args = sdk_utils.subprocess_args(self.pod)
         self.assertEqual(args['env']['GROW_ENVIRONMENT_NAME'], 'testing')
+
+    def test_subprocess_args_no_env_name(self):
+        """Test subprocess args without env name."""
+        pod = mocks.mock_pod(root='/testing/')
+        args = sdk_utils.subprocess_args(pod)
+        self.assertFalse('GROW_ENVIRONMENT_NAME' in args['env'])
 
     def test_subprocess_shell(self):
         """Test subprocess args with shell."""
