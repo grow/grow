184c50ef72780a61cc9dcaf8c91371a530ede734
Randy Merrill <randy@blinkk.com>
2018-08-14 11:37:55 -0600

    Adding more tests for the `run_installers`.

--- grow/sdk/installer_test.py
+++ grow/sdk/installer_test.py
@@ -1,7 +1,12 @@
 """Tests for Base Installer."""
 
 import unittest
+import mock
+from grow.common import base_config
 from grow.sdk import installer
+from grow.sdk.installers import base_installer
+from grow.sdk.installers import npm_installer
+from grow.testing import mocks
 
 
 class InstallerTestCase(unittest.TestCase):
@@ -22,6 +27,38 @@ class InstallerTestCase(unittest.TestCase):
             'Bring Me Another Shrubbery!', extras=['1', 'A'])
         expected = u'\x1b[38;5;1m\x1b[1m[✘] Bring Me Another Shrubbery!\n   1\n   A\x1b[0m'
         self.assertEqual(expected, message)
+
+    @mock.patch('subprocess.Popen')
+    @mock.patch('subprocess.call')
+    def test_run_installers(self, _mock_call, mock_popen):
+        """Run installers successfully."""
+        config = base_config.BaseConfig()
+        mock_process = mock.Mock()
+        mock_process.wait.return_value = None
+        mock_popen.return_value = mock_process
+        env = mocks.mock_env(name="testing")
+        pod = mocks.mock_pod(env=env, root='/testing/')
+        pod.file_exists.return_value = True
+        self.installer = installer.Installer([
+            npm_installer.NpmInstaller(pod, config),
+        ])
+        self.assertTrue(self.installer.run_installers())
+
+    @mock.patch('subprocess.Popen')
+    @mock.patch('subprocess.call')
+    def test_run_installers_dependency_fail(self, mock_call, _mock_popen):
+        """Run installers with failing dependency check."""
+        config = base_config.BaseConfig()
+        logger = mock.Mock()
+        mock_call.return_value = 127  # Simulate Yarn not found.
+        env = mocks.mock_env(name="testing")
+        pod = mocks.mock_pod(env=env, root='/testing/')
+        pod.file_exists.return_value = True
+        self.installer = installer.Installer([
+            npm_installer.NpmInstaller(pod, config),
+        ], logger=logger)
+        self.assertFalse(self.installer.run_installers())
+        logger.error.assert_called()
 
     def test_success(self):
         """Success message formatting."""
