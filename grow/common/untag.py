93daa9e98e749a34a142536793b14f6f072918d1
Randy Merrill <randy@blinkk.com>
2018-09-26 12:27:30 -0600

    Adding basics for untagging params with callables.

--- grow/common/untag.py
+++ grow/common/untag.py
@@ -15,11 +15,21 @@ class Untag(object):
         """Untags fields, handling translation priority."""
         paths_to_keep_tagged = set()
 
+        # When untagging the order of the keys isn't consistent. Sometimes the
+        # tagged value is found but then is overwritten by the original value
+        # since it is processed after the tagged version. Need to keep track of
+        # untagged keys to make sure that they are not overwritten by the original.
+        untagged_key_paths = set()
+
         # pylint: disable=too-many-return-statements
         def _visit(path, key, value):
             """Function for each key and value in the data."""
             if not isinstance(key, str):
                 return key, value
+
+            # If the key has already been untagged, don't overwrite.
+            if (path, key) in untagged_key_paths:
+                return False
 
             if key.endswith('@#'):
                 # Translation Comment.
@@ -41,7 +51,8 @@ class Untag(object):
                     if not params[param_key]:
                         return False
                     return params[param_key](
-                        data, untagged_key, param_key, param_value, value)
+                        data, untagged_key, param_key, param_value, value,
+                        locale_identifier=locale_identifier)
 
             # Support <key>@<locale regex>: <value>.
             match = LOCALIZED_KEY_REGEX.match(key)
@@ -51,6 +62,7 @@ class Untag(object):
             locale_regex = r'^{}$'.format(locale_from_key)
             if marked_for_extraction or not locale_identifier or not re.match(locale_regex, locale_identifier):
                 return False
+            untagged_key_paths.add((path, untagged_key))
             return untagged_key, value
 
         # Backwards compatibility for https://github.com/grow/grow/issues/95
@@ -77,7 +89,7 @@ class Untag(object):
 class UntagParam(object):
     """Untagging param for complex untagging."""
 
-    def __call__(self, data, untagged_key, param_key, param_value, value):
+    def __call__(self, data, untagged_key, param_key, param_value, value, locale_identifier=None):
         raise NotImplementedError()
 
 
@@ -87,10 +99,58 @@ class UntagParamRegex(object):
     def __init__(self, value):
         self.value = value
 
-    def __call__(self, data, untagged_key, param_key, param_value, value):
+    def __call__(self, data, untagged_key, param_key, param_value, value, locale_identifier=None):
         if not self.value:
             return False
         value_regex = r'^{}$'.format(param_value)
         if not re.match(value_regex, self.value):
+            return False
+        return untagged_key, value
+
+
+class UntagParamFieldRegex(object):
+    """Param using the value a document field with fallback as a regex to match.
+
+    Attempts to use the value of one of the other data fields as a regex.
+    If there is no matching key found it falls back to the collection or podspec.
+    """
+
+    def __init__(self, podspec_data, collection_data, value):
+        self.podspec_data = podspec_data
+        self.collection_data = collection_data
+        self.value = value
+
+    def __call__(self, data, untagged_key, param_key, param_value, value, locale_identifier=None):
+        podspec_value = self.podspec_data.get(param_value, None)
+        collection_value = self.collection_data.get(param_value, podspec_value)
+        regex_value = data.get(param_value, collection_value)
+        if not regex_value:
+            return False
+        value_regex = r'^{}$'.format(regex_value)
+        if not re.match(value_regex, self.value):
+            return False
+        return untagged_key, value
+
+
+class UntagParamLocaleRegex(object):
+    """Param using a document field as a regex group to match locale.
+
+    Attempts to use the value of one of the other data fields as a locale regex.
+    If there is no matching key found it falls back to the collection or podspec.
+    """
+
+    def __init__(self, pod, collection):
+        self.podspec_data = pod.podspec.get('localization', {})
+        self.collection_data = collection.get('localization', {})
+
+    def __call__(self, data, untagged_key, param_key, param_value, value, locale_identifier=None):
+        podspec_value = self.podspec_data.get(param_value, None)
+        collection_value = self.collection_data.get(param_value, podspec_value)
+        regex_value = data.get(param_value, collection_value)
+        if not regex_value:
+            return False
+
+        value_regex = r'^{}$'.format(regex_value)
+        if not re.match(value_regex, locale_identifier):
             return False
         return untagged_key, value
