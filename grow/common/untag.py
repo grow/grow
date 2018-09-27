eae0c1e9df9e102b1aa575934c92ddebe1bad48b
Randy Merrill <randy@blinkk.com>
2018-09-27 12:11:27 -0600

    Fixing to correctly reference the group.

--- grow/common/untag.py
+++ grow/common/untag.py
@@ -112,8 +112,8 @@ class UntagParamLocaleRegex(object):
     def __call__(self, data, untagged_key, param_key, param_value, value, locale_identifier=None):
         podspec_value = self.podspec_data.get(param_value, None)
         collection_value = self.collection_data.get(param_value, podspec_value)
-        localization = data.get('$localization', {})
-        regex_value = localization.get(param_value, collection_value)
+        local_value = data.get('$localization', {}).get('groups', {})
+        regex_value = local_value.get(param_value, collection_value)
         if not regex_value:
             return False
 
