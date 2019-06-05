9f043df69ce50925097862ffb762e0a401f670f8
Randy Merrill <randy@blinkk.com>
2019-06-05 16:02:15 -0600

    Adding profiler to the install command.

--- grow/commands/install.py
+++ grow/commands/install.py
@@ -6,9 +6,11 @@ from grow.commands import result
 from grow.commands import shared
 from grow.common import base_config
 from grow.common import rc_config
+from grow.performance import profile
 from grow.pod import pod as grow_pod
 from grow.sdk import installer
 from grow.sdk import installers
+from grow.storage import local as local_storage
 
 
 CFG = rc_config.RC_CONFIG.prefixed('grow.install')
@@ -19,16 +21,17 @@ CFG = rc_config.RC_CONFIG.prefixed('grow.install')
 @shared.pod_path_argument
 def install(pod_path):
     """Installs local dependencies for such as extensions, npm, yarn, etc."""
-    cmd_result = result.CommandResult()
+    profiler = profile.Profile()
+    with profiler('grow.install'):
+        root_path = os.path.abspath(os.path.join(os.getcwd(), pod_path))
+        storage = local_storage.LocalStorage(root_path)
+        pod = grow_pod.Pod(root_path, storage=storage, profiler=profiler)
 
-    root_path = os.path.abspath(os.path.join(os.getcwd(), pod_path))
-    pod = grow_pod.Pod(root_path)
+        config = base_config.BaseConfig()
+        built_in_installers = []
+        for installer_class in installers.BUILT_IN_INSTALLERS:
+            built_in_installers.append(installer_class(storage, config))
+        grow_installer = installer.Installer(built_in_installers, pod)
+        grow_installer.run_installers()
 
-    config = base_config.BaseConfig()
-    built_in_installers = []
-    for installer_class in installers.BUILT_IN_INSTALLERS:
-        built_in_installers.append(installer_class(pod, config))
-    grow_installer = installer.Installer(built_in_installers, pod)
-    grow_installer.run_installers()
-
-    return cmd_result
+    return result.CommandResult(pod=pod, profiler=profiler)
