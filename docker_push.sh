40bc272f4a594aaa20582179a3c50117e0c4c559
Randy Merrill <randy@blinkk.com>
2019-06-07 10:14:54 -0600

    Updating docker push process to either do docker or gcr.io, not both.

--- docker_push.sh
+++ docker_push.sh
@@ -4,7 +4,13 @@ set -e
 # Also needs to be updated in Dockerfile when changed.
 GROW_VERSION=`cat grow/VERSION`
 
-echo "Building and Pushing Grow $GROW_VERSION to Docker Hub"
+if [ "$1" == "gcr.io" ]; then
+  echo "Building and Pushing Grow $GROW_VERSION to gcr.io"
+  echo "To build and push to Docker run 'docker_push.sh'"
+else
+  echo "Building and Pushing Grow $GROW_VERSION to Docker Hub"
+  echo "To build and push to gcr.io run 'docker_push.sh gcr.io'"
+fi
 
 # Ubuntu Base.
 docker build --no-cache --build-arg grow_version=$GROW_VERSION \
@@ -15,17 +21,17 @@ docker build --no-cache --build-arg grow_version=$GROW_VERSION \
 docker run --rm=true --workdir=/tmp -i grow/base:$GROW_VERSION  \
   bash -c "git clone https://github.com/grow/grow.io.git && cd grow.io/ && grow install && grow build"
 
-docker push grow/base:$GROW_VERSION
-docker push grow/base:latest
-
-# Legacy docker image support.
-docker push grow/baseimage:$GROW_VERSION
-docker push grow/baseimage:latest
-
 # Google cloud registry.
 if [ "$1" == "gcr.io" ]; then
   docker push gcr.io/grow-prod/base:$GROW_VERSION
   docker push gcr.io/grow-prod/base:latest
+else
+  docker push grow/base:$GROW_VERSION
+  docker push grow/base:latest
+
+  # Legacy docker image support.
+  docker push grow/baseimage:$GROW_VERSION
+  docker push grow/baseimage:latest
 fi
 
 # Ubuntu Command.
@@ -36,13 +42,13 @@ docker build --no-cache --build-arg grow_version=$GROW_VERSION \
 
 docker run grow/grow:$GROW_VERSION --version
 
-docker push grow/grow:$GROW_VERSION
-docker push grow/grow:latest
-
 # Google cloud registry.
 if [ "$1" == "gcr.io" ]; then
   docker push gcr.io/grow-prod/grow:$GROW_VERSION
   docker push gcr.io/grow-prod/grow:latest
+else
+  docker push grow/grow:$GROW_VERSION
+  docker push grow/grow:latest
 fi
 
 # Alpine Base.
@@ -54,13 +60,13 @@ docker build --no-cache --build-arg grow_version=$GROW_VERSION \
 docker run --rm=true --workdir=/tmp -i grow/base:$GROW_VERSION-alpine  \
   bash -c "git clone https://github.com/grow/grow.io.git && cd grow.io/ && grow install && grow build"
 
-docker push grow/base:$GROW_VERSION-alpine
-docker push grow/base:alpine-latest
-
 # Google cloud registry.
 if [ "$1" == "gcr.io" ]; then
   docker push gcr.io/grow-prod/base:$GROW_VERSION-alpine
   docker push gcr.io/grow-prod/base:alpine-latest
+else
+  docker push grow/base:$GROW_VERSION-alpine
+  docker push grow/base:alpine-latest
 fi
 
 # Alpine Command.
@@ -71,11 +77,12 @@ docker build --no-cache --build-arg grow_version=$GROW_VERSION \
 
 docker run grow/grow:$GROW_VERSION-alpine --version
 
-docker push grow/grow:$GROW_VERSION-alpine
-docker push grow/grow:alpine-latest
 
 # Google cloud registry.
 if [ "$1" == "gcr.io" ]; then
   docker push gcr.io/grow-prod/grow:$GROW_VERSION-alpine
   docker push gcr.io/grow-prod/grow:alpine-latest
+else
+  docker push grow/grow:$GROW_VERSION-alpine
+  docker push grow/grow:alpine-latest
 fi
