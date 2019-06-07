5cf67e665db6b71a686e53e5d9ee0308a737ea14
Randy Merrill <randy@blinkk.com>
2019-06-07 15:42:48 -0600

    Removing the docker exec.
    When using subprocess calls in python it doesn't work with the endpoint docker configuration.

--- docker_push.sh
+++ docker_push.sh
@@ -21,34 +21,18 @@ docker build --no-cache --build-arg grow_version=$GROW_VERSION \
 docker run --rm=true --workdir=/tmp -i grow/base:$GROW_VERSION  \
   bash -c "git clone https://github.com/grow/grow.io.git && cd grow.io/ && grow install && grow build"
 
-# Google cloud registry.
 if [ "$1" == "gcr.io" ]; then
+  # Google cloud registry.
   docker push gcr.io/grow-prod/base:$GROW_VERSION
   docker push gcr.io/grow-prod/base:latest
 else
+  # Docker Hub.
   docker push grow/base:$GROW_VERSION
   docker push grow/base:latest
 
   # Legacy docker image support.
   docker push grow/baseimage:$GROW_VERSION
   docker push grow/baseimage:latest
-fi
-
-# Ubuntu Command.
-docker build --no-cache --build-arg grow_version=$GROW_VERSION \
-  -t grow/grow:$GROW_VERSION -t grow/grow:latest \
-  -t gcr.io/grow-prod/grow:$GROW_VERSION -t gcr.io/grow-prod/grow:latest \
-  -f Dockerfile.exec .
-
-docker run grow/grow:$GROW_VERSION --version
-
-# Google cloud registry.
-if [ "$1" == "gcr.io" ]; then
-  docker push gcr.io/grow-prod/grow:$GROW_VERSION
-  docker push gcr.io/grow-prod/grow:latest
-else
-  docker push grow/grow:$GROW_VERSION
-  docker push grow/grow:latest
 fi
 
 # Alpine Base.
@@ -60,29 +44,12 @@ docker build --no-cache --build-arg grow_version=$GROW_VERSION \
 docker run --rm=true --workdir=/tmp -i grow/base:$GROW_VERSION-alpine  \
   bash -c "git clone https://github.com/grow/grow.io.git && cd grow.io/ && grow install && grow build"
 
-# Google cloud registry.
 if [ "$1" == "gcr.io" ]; then
+  # Google cloud registry.
   docker push gcr.io/grow-prod/base:$GROW_VERSION-alpine
   docker push gcr.io/grow-prod/base:alpine-latest
 else
+  # Docker Hub.
   docker push grow/base:$GROW_VERSION-alpine
   docker push grow/base:alpine-latest
-fi
-
-# Alpine Command.
-docker build --no-cache --build-arg grow_version=$GROW_VERSION \
-  -t grow/grow:$GROW_VERSION-alpine -t grow/grow:alpine-latest \
-  -t gcr.io/grow-prod/grow:$GROW_VERSION-alpine -t gcr.io/grow-prod/grow:alpine-latest \
-  -f Dockerfile.exec .
-
-docker run grow/grow:$GROW_VERSION-alpine --version
-
-
-# Google cloud registry.
-if [ "$1" == "gcr.io" ]; then
-  docker push gcr.io/grow-prod/grow:$GROW_VERSION-alpine
-  docker push gcr.io/grow-prod/grow:alpine-latest
-else
-  docker push grow/grow:$GROW_VERSION-alpine
-  docker push grow/grow:alpine-latest
 fi
