eee49d94c1349ec0276988398a2c96c4b700cb7a
Randy Merrill <randy@blinkk.com>
2019-07-10 10:29:12 -0600

    Adding the ability to skip install for npm and extensions when no changes.
    Adds a hash file when the install is completed to the directories.
    
    When CI builds restore caches, the install command should find the hash file and use it to determine if there is a need to run the install.
    
    Fixes #978

--- grow/sdk/installers/extensions_installer.py
+++ grow/sdk/installers/extensions_installer.py
@@ -4,10 +4,17 @@ import subprocess
 from grow.sdk.installers import base_installer
 
 
+INSTALL_HASH_FILE_NAME = 'install-hash.txt'
+
+
 class ExtensionsInstaller(base_installer.BaseInstaller):
     """Grow extensions installer."""
 
     KIND = 'extensions'
+
+    def _generate_hashes(self):
+        """Create hash string based on the package and lock files."""
+        return self.pod.hash_file(self.extensions_filename)
 
     @property
     def post_install_messages(self):
@@ -15,9 +22,32 @@ class ExtensionsInstaller(base_installer.BaseInstaller):
         return ['Finished: Extensions -> {}/'.format(self.pod.extensions_dir)]
 
     @property
+    def extensions_filename(self):
+        """Filename for the install hash."""
+        return '/{}'.format(self.pod.FILE_EXTENSIONS)
+
+    @property
+    def install_hash_filename(self):
+        """Filename for the install hash."""
+        return '/{}/{}'.format(self.pod.extensions_dir, INSTALL_HASH_FILE_NAME)
+
+    @property
     def should_run(self):
         """Should the installer run?"""
-        return self.pod.file_exists('/{}'.format(self.pod.FILE_EXTENSIONS))
+        if not self.pod.file_exists(self.extensions_filename):
+            return False
+
+        if self.config.get('force', None):
+            return True
+
+        if self.pod.file_exists(self.install_hash_filename):
+            existing_hash = self.pod.read_file(self.install_hash_filename)
+            current_hash = self._generate_hashes()
+            if existing_hash == current_hash:
+                self.pod.logger.info('Skipping extensions install since no changes detected.')
+                self.pod.logger.info('   Use `grow install -f` to force install.')
+                return False
+        return True
 
     def check_prerequisites(self):
         """Check if required prerequisites are installed or available."""
@@ -40,6 +70,7 @@ class ExtensionsInstaller(base_installer.BaseInstaller):
             install_command, **self.subprocess_args(shell=True))
         code = process.wait()
         if not code:
+            self.pod.write_file(self.install_hash_filename, self._generate_hashes())
             return
         raise base_installer.InstallError(
             'There was an error running `{}`.'.format(install_command))
