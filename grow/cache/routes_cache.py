3989111adc2091569d4e9cfd0805af97f924ce98
Randy Merrill <randy@blinkk.com>
2019-07-17 15:02:17 -0600

    Adding a routes cache class for working with routing.

---
+++ grow/cache/routes_cache.py
@@ -0,0 +1,90 @@
+"""
+Cache for storing and retrieving routing information in a pod.
+"""
+
+FILE_ROUTES_CACHE = 'routescache.json'
+EXPORT_KEY = 'routes'
+
+
+class RoutesCache(object):
+    """Routes cache for caching routing data."""
+
+    def __init__(self):
+        self._cache = {}
+        self._is_dirty = False
+
+    @staticmethod
+    def _export_cache(routes):
+        routing_info = {}
+        for env, env_routes in routes.items():
+            routing_info[env] = {}
+            for path, item in env_routes.items():
+                try:
+                    value = item['value'].export()
+                except AttributeError:
+                    value = item['value']
+                routing_info[env][path] = {
+                    'value': value,
+                    'options': item['options'],
+                }
+        return routing_info
+
+    def add(self, key, value, options=None, env=None):
+        """Add a new item to the cache or overwrite an existing value."""
+        if env not in self._cache:
+            self._cache[env] = {}
+        cache = self._cache[env]
+        cache_value = {
+            'value': value,
+            'options': options,
+        }
+        if not self._is_dirty and (key not in cache or cache[key] != cache_value):
+            self._is_dirty = True
+        cache[key] = cache_value
+
+    def export(self):
+        """Returns the raw cache data."""
+        return {
+            'version': 1,
+            EXPORT_KEY: self._export_cache(self._cache),
+        }
+
+    def from_data(self, data, generator):
+        """Set the cache from data."""
+        # Check for version changes in the data format.
+        version = data.get('version')
+        if not version or version < 1:
+            return
+
+        for env, env_data in data[EXPORT_KEY].items():
+            for key, item in env_data.items():
+                self.add(
+                    key, generator(**item['value']),
+                    options=item['options'], env=env)
+
+    def get(self, key, env=None):
+        """Retrieve the value from the cache."""
+        return self._cache.get(env, {}).get(key, None)
+
+    @property
+    def is_dirty(self):
+        """Have the contents of the object cache been modified?"""
+        return self._is_dirty
+
+    def mark_clean(self):
+        """Mark that the object cache is clean."""
+        self._is_dirty = False
+
+    def raw(self, env=None):
+        """Returns the raw cache data."""
+        return self._cache.get(env, {})
+
+    def remove(self, key, env=None):
+        """Removes a single element from the cache."""
+        self._is_dirty = True
+        return self._cache.get(env, {}).pop(key, None)
+
+    def reset(self):
+        """Reset the internal cache object."""
+        self._cache = {}
+        self._is_dirty = False
---
+++ grow/cache/routes_cache_test.py
@@ -0,0 +1,97 @@
+"""Tests for the routes cache."""
+
+import unittest
+from grow.cache import routes_cache
+
+
+class RoutesCacheTestCase(unittest.TestCase):
+    """Tests for the routes cache."""
+
+    def setUp(self):
+        self.routes_cache = routes_cache.RoutesCache()
+
+    def test_add(self):
+        """Adding a value to the cache."""
+        self.assertFalse(self.routes_cache.is_dirty)
+        self.routes_cache.add('answer', 42)
+        self.assertEqual(42, self.routes_cache.get('answer')['value'])
+        self.assertTrue(self.routes_cache.is_dirty)
+
+    def test_add_clean(self):
+        """Adding a value to the cache with the same value does not make dirty."""
+        self.assertFalse(self.routes_cache.is_dirty)
+        self.routes_cache.add('answer', 42)
+        self.assertEqual(42, self.routes_cache.get('answer')['value'])
+        self.assertTrue(self.routes_cache.is_dirty)
+        self.routes_cache.mark_clean()
+        self.assertFalse(self.routes_cache.is_dirty)
+        self.routes_cache.add('answer', 42)
+        self.assertFalse(self.routes_cache.is_dirty)
+
+    def test_add_dirty(self):
+        """Adding a value to the cache with the different value makes dirty."""
+        self.assertFalse(self.routes_cache.is_dirty)
+        self.routes_cache.add('answer', 42)
+        self.assertEqual(42, self.routes_cache.get('answer')['value'])
+        self.assertTrue(self.routes_cache.is_dirty)
+        self.routes_cache.mark_clean()
+        self.assertFalse(self.routes_cache.is_dirty)
+        self.routes_cache.add('answer', 2)
+        self.assertTrue(self.routes_cache.is_dirty)
+
+    def test_export(self):
+        """Raw cache export."""
+        self.routes_cache.add('question', {
+            'answer': 42
+        })
+        self.routes_cache.add('answer', {
+            'question': '???'
+        })
+        self.assertDictEqual({
+            routes_cache.EXPORT_KEY: {
+                None: {
+                    'answer': {
+                        'options': None,
+                        'value': {
+                            'question': '???'
+                        },
+                    },
+                    'question': {
+                        'options': None,
+                        'value': {
+                            'answer': 42
+                        },
+                    },
+                },
+            },
+            'version': 1,
+        }, self.routes_cache.export())
+
+    def test_mark_clean(self):
+        """Can mark as clean?"""
+        self.assertFalse(self.routes_cache.is_dirty)
+        self.routes_cache.add('answer', 42)
+        self.assertTrue(self.routes_cache.is_dirty)
+        self.routes_cache.mark_clean()
+        self.assertFalse(self.routes_cache.is_dirty)
+
+    def test_remove(self):
+        """Test removing a key from the cache."""
+        value = {
+            'answer': 42
+        }
+        self.routes_cache.add('question', value)
+        self.assertEqual(42, self.routes_cache.get('question')['value']['answer'])
+        self.routes_cache.mark_clean()
+        self.assertFalse(self.routes_cache.is_dirty)
+        self.assertEqual(
+            {
+                'options': None,
+                'value': value,
+            }, self.routes_cache.remove('question'))
+        self.assertEqual(None, self.routes_cache.get('question'))
+        self.assertTrue(self.routes_cache.is_dirty)
+
+
+if __name__ == '__main__':
+    unittest.main()
