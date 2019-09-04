b7dc818ee705286bb26605276065ac9a092792e6
Randy Merrill <randy@blinkk.com>
2019-09-04 12:09:06 -0600

    Removing old bower installer.

--- grow/sdk/installers/__init__.py
+++ grow/sdk/installers/__init__.py
@@ -1,6 +1,5 @@
 """Grow prerequisite installers."""
 
-from grow.sdk.installers import bower_installer
 from grow.sdk.installers import extensions_installer
 from grow.sdk.installers import gulp_installer
 from grow.sdk.installers import npm_installer
@@ -11,6 +10,5 @@ BUILT_IN_INSTALLERS = [
     extensions_installer.ExtensionsInstaller,
     nvm_installer.NvmInstaller,  # Before any node based installers.
     npm_installer.NpmInstaller,
-    bower_installer.BowerInstaller,  # After the npm install.
     gulp_installer.GulpInstaller,  # After the npm install.
 ]
