4b65a9bf013bebab6a5c6aba4ba41fba09df5a05
Randy Merrill <randy@blinkk.com>
2019-11-15 15:13:06 -0700

    Adding a hook for adding static configs.

--- grow/extensions/hooks/__init__.py
+++ grow/extensions/hooks/__init__.py
@@ -6,11 +6,13 @@ from . import post_render_hook
 from . import pre_deploy_hook
 from . import pre_render_hook
 from . import pre_process_hook
+from . import podspec_static_dir_hook
 from . import router_add_hook
 
 # pylint: disable=invalid-name
 DevFileChangeHook = dev_file_change_hook.DevFileChangeHook
 DevHandlerHook = dev_handler_hook.DevHandlerHook
+PodspecStaticDirHook = podspec_static_dir_hook.PodspecStaticDirHook
 PostRenderHook = post_render_hook.PostRenderHook
 PreDeployHook = pre_deploy_hook.PreDeployHook
 PreRenderHook = pre_render_hook.PreRenderHook
@@ -21,6 +23,7 @@ RouterAddHook = router_add_hook.RouterAddHook
 HOOKS = (
     dev_file_change_hook.DevFileChangeHook,
     dev_handler_hook.DevHandlerHook,
+    podspec_static_dir_hook.PodspecStaticDirHook,
     post_render_hook.PostRenderHook,
     pre_deploy_hook.PreDeployHook,
     pre_render_hook.PreRenderHook,
