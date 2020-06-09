ad860965c973ca571b3f52f6e4596c951ac202f5
Randy Merrill <randy@blinkk.com>
2020-06-08 20:03:53 -0600

    Fixing the hook arguments.

--- grow/extensions/hooks/dev_manager_message_hook.py
+++ grow/extensions/hooks/dev_manager_message_hook.py
@@ -9,7 +9,8 @@ class DevManagerMessageHook(base_hook.BaseHook):
     KEY = 'dev_manager_message'
     NAME = 'Dev Manager Message Handler'
 
+    # display_func(label, message, label_color=None, message_color=None)
     # pylint: disable=arguments-differ,unused-argument
-    def trigger(self, previous_result, first_column_width, url, *_args, **_kwargs):
+    def trigger(self, previous_result, display_func, url_base, url_root, *_args, **_kwargs):
         """Trigger the dev handler hook."""
         return previous_result
